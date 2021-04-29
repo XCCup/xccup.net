b904161c922a152b480369dae6ea0e80dbfc6cef
Stephan Schöpe <socialcrap+github@posteo.de>
2021-04-29 12:21:43 +0200

    Upload Form max width

--- Frontend/src/components/UploadForm.vue
+++ Frontend/src/components/UploadForm.vue
@@ -1,109 +1,107 @@
 <template>
-  <div class="flug-melden mb-5">
-    <div id="upload" class="container">
-      <h3>Flug hochladen</h3>
-      <form @submit.prevent="sendForm">
-        <div class="mb-3">
-          <label for="formFile" class="form-label">.igc auswählen</label>
-          <input class="form-control" type="file" id="formFile" />
-        </div>
+  <div id="upload" class="container">
+    <h3>Flug hochladen</h3>
+    <form @submit.prevent="sendForm">
+      <div class="mb-3">
+        <label for="formFile" class="form-label">.igc auswählen</label>
+        <input class="form-control" type="file" id="formFile" />
+      </div>
 
+      <div class="row">
+        <div class="col">
+          <BaseInput
+            v-model="flight.takeoff"
+            label="Startplatz"
+            :isDisabled="true"
+          />
+        </div>
+        <div class="col">
+          <BaseInput
+            v-model="flight.landing"
+            label="Landeplatz"
+            :isDisabled="true"
+          />
+        </div>
+      </div>
+      <div class="col-md-12">
         <div class="row">
-          <div class="col">
-            <BaseInput
-              v-model="flight.takeoff"
-              label="Startplatz"
-              :isDisabled="true"
+          <span>Fluggerät</span>
+          <div class="col-md-9">
+            <BaseSelect
+              v-model="flight.glider"
+              label="Fluggerät"
+              :showLabel="false"
+              :options="[flight.glider]"
             />
           </div>
-          <div class="col">
-            <BaseInput
-              v-model="flight.landing"
-              label="Landeplatz"
-              :isDisabled="true"
-            />
+          <div class="col-md-3">
+            <div class="d-grid gap-2">
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#addAircraftModal"
+              >
+                Hinzufügen
+              </button>
+            </div>
           </div>
         </div>
-        <div class="col-md-12">
-          <div class="row mt-2">
-            <span>Fluggerät</span>
-            <div class="col-md-9">
-              <BaseSelect
-                v-model="flight.glider"
-                label="Fluggerät"
-                :showLabel="false"
-                :options="[flight.glider]"
-              />
-            </div>
-            <div class="col-md-3">
-              <div class="d-grid gap-2">
-                <button
-                  type="button"
-                  class="btn btn-primary"
-                  data-bs-toggle="modal"
-                  data-bs-target="#addAircraftModal"
-                >
-                  Hinzufügen
-                </button>
-              </div>
-            </div>
-          </div>
+      </div>
+
+      <div class="my-3">
+        <div class="form-floating mb-3">
+          <textarea
+            class="form-control"
+            placeholder="Flugbericht"
+            id="floatingTextarea2"
+            style="height: 100px"
+          ></textarea>
+          <label for="floatingTextarea2">Flugbericht</label>
         </div>
 
         <div class="mb-3">
-          <div class="form-floating mb-3">
-            <textarea
-              class="form-control"
-              placeholder="Flugbericht"
-              id="floatingTextarea2"
-              style="height: 100px"
-            ></textarea>
-            <label for="floatingTextarea2">Flugbericht</label>
-          </div>
-
-          <div class="mb-3">
-            <label for="formFileMultiple" class="form-label"
-              >Bilder hinzufügen</label
-            >
-            <input
-              class="form-control"
-              type="file"
-              id="formFileMultiple"
-              multiple
-            />
-          </div>
-
-          <div class="form-check mb-3">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label class="form-check-label" for="flexCheckDefault">
-              Die Ausschreibung ist mir bekannt, flugrechtliche Auflagen wurden
-              eingehalten.<br />
-              Jeder Teilnehmer nimmt auf eigene Gefahr an diesem Wettbewerb
-              teil. Ansprüche gegenüber dem Veranstalter, dem Ausrichter, dem
-              Organisator, dem Wettbewerbsleiter sowie deren Helfer wegen
-              einfacher Fahrlässigkeit sind ausgeschlossen. Mit dem Anklicken
-              des Häckchens erkenne ich die
-              <a href="#">Ausschreibung</a> und
-              <a href="#">Datenschutzbestimmungen</a>
-              an.
-            </label>
-          </div>
-
-          <button type="submit" class="btn btn-primary me-1">
-            Streckenmeldung absenden
-          </button>
-          <button type="submit" class="btn btn-danger">Abbrechen</button>
+          <label for="formFileMultiple" class="form-label"
+            >Bilder hinzufügen</label
+          >
+          <input
+            class="form-control"
+            type="file"
+            id="formFileMultiple"
+            multiple
+          />
         </div>
-      </form>
-    </div>
-    {{ flight }}
-    <AddAircraftModal />
+
+        <div class="form-check mb-3">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label class="form-check-label" for="flexCheckDefault">
+            Die Ausschreibung ist mir bekannt, flugrechtliche Auflagen wurden
+            eingehalten.<br />
+            Jeder Teilnehmer nimmt auf eigene Gefahr an diesem Wettbewerb teil.
+            Ansprüche gegenüber dem Veranstalter, dem Ausrichter, dem
+            Organisator, dem Wettbewerbsleiter sowie deren Helfer wegen
+            einfacher Fahrlässigkeit sind ausgeschlossen. Mit dem Anklicken des
+            Häckchens erkenne ich die
+            <a href="#">Ausschreibung</a> und
+            <a href="#">Datenschutzbestimmungen</a>
+            an.
+          </label>
+        </div>
+
+        <button type="submit" class="btn btn-primary me-1">
+          Streckenmeldung absenden
+        </button>
+        <button type="submit" class="btn btn-danger">Abbrechen</button>
+      </div>
+    </form>
   </div>
+  {{ flight }}
+  <AddAircraftModal />
 </template>
 
 <script>
@@ -143,4 +141,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  max-width: 900px !important;
+}
+</style>
