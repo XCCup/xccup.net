6eb4896c0f5d696bafeba439c750919af17df6d1
Stephan Schöpe <socialcrap+github@posteo.de>
2021-05-03 19:43:08 +0200

    comment

--- Backend/docker-entrypoint.sh
+++ Backend/docker-entrypoint.sh
@@ -6,6 +6,7 @@ npm uninstall bcrypt
 # install the bcrypt modules for the machine
 # This is needed because bcrypt uses different modules for different architectures.
 # If the node_modules folder is shared the process will crash otherwise.
+# https://medium.com/hacktive-devs/the-bcrypt-bg-on-docker-9bc36cc7f684
 
 npm install bcrypt
 
