0d370585124f1e70084466d85a85b41b850a87ae
Stephan Schöpe <socialcrap+github@posteo.de>
2021-05-04 18:46:19 +0200

    docker tuning

--- Backend/docker-entrypoint.sh
+++ Backend/docker-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 # uninstall the current bcrypt modules
-npm uninstall bcrypt
+# npm uninstall bcrypt
 
 # install the bcrypt modules for the machine
 # This is needed because bcrypt uses different modules for different architectures.
 # If the node_modules folder is shared the process will crash otherwise.
 # https://medium.com/hacktive-devs/the-bcrypt-bg-on-docker-9bc36cc7f684
 
-npm install bcrypt
+# npm install bcrypt
 
 echo "Waiting for DB to start..."
 ./wait-for db:5432 
 
-
 echo "Starting the API server..."
-npm run dev 
+if [ $NODE_ENV == "production" ]
+then
+    # https://github.com/nodejs/docker-node/blob/main/docs/BestPractices.md#cmd
+    node app.js 
+else
+    npm run dev
+fi
