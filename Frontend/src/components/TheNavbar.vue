457b2d7eba0146344c026aa1a1fbe67dd50138fc
Stephan Schöpe <socialcrap+github@posteo.de>
2021-05-13 19:00:44 +0200

    catch undefined

--- Frontend/src/components/TheNavbar.vue
+++ Frontend/src/components/TheNavbar.vue
@@ -69,7 +69,7 @@
             data-bs-toggle="dropdown"
           >
             <i class="bi bi-person"></i>
-            {{ authUser.name }}
+            {{ authUser?.name ?? "Default User" }}
           </button>
           <div class="dropdown-menu" style="width: 250px">
             <form class="px-4 py-3">
