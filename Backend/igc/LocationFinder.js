428d5bc2a26e3f5f82ebee67945ce57e61691a43
Stephan Schöpe <socialcrap+github@posteo.de>
2021-05-23 19:49:57 +0200

    Changed to only town name

--- Backend/igc/LocationFinder.js
+++ Backend/igc/LocationFinder.js
@@ -2,14 +2,19 @@
 const axios = require("axios");
 
 const byLatLong = async (location) => {
-  // const location = "50.20524528622611,7.064103332484184";
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location}&language=de&result_type=locality&key=${process.env.GOOGLE_MAPS_API_KEY}`;
-  const result = await axios.get(url);
-  // ZipCode TownName, CountryName
-  const nameOfTown = result.data.results[0].formatted_address;
-  // Only TownName
-  // const nameOfTown = result.data.results[0].address_components[0].long_name;
-  return nameOfTown;
+  try {
+    // const location = "50.20524528622611,7.064103332484184";
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location}&language=de&result_type=locality&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+    console.log(url);
+    const result = await axios.get(url);
+    // ZipCode TownName, CountryName
+    // const nameOfTown = result.data.results[0].formatted_address;
+    // Only TownName
+    const nameOfTown = result.data.results[0].address_components[0].long_name;
+    return nameOfTown;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const findTakeoffAndLanding = async (takeoffFix, landingFix) => {
