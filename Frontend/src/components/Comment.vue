d5afc32ad89dbd4aa93d43e6e9d5176477c965cd
Stephan Schöpe <socialcrap+github@posteo.de>
2021-09-29 21:54:47 +0200

    API adjustments

--- Frontend/src/components/Comment.vue
+++ Frontend/src/components/Comment.vue
@@ -7,7 +7,7 @@
       height="24"
       width="24"
     />
-    <a href="#">{{ comment.name ? comment.name : "Pilot Name" }}</a>
+    <a href="#">{{ comment.User.name }}</a>
     <span class="ms-auto fw-light text-secondary"
       ><BaseDate :timestamp="comment.createdAt" dateFormat="dd.MM.yyyy"
     /></span>
