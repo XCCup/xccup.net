4871563aca617508cd667834ae94fe1893b02f63
Kai Wissel <kai.wpkt@gmail.com>
2021-10-01 12:26:32 +0200

    Added google elevation api

--- Backend/test/ElevationAttacher.test.js
+++ Backend/test/ElevationAttacher.test.js
@@ -1,39 +1,54 @@
 /**
  * @jest-environment node
  */
-const { readIgcFile } = require("./IgcTestHelper");
+const Helper = require("./IgcTestHelper");
 const IGCParser = require("igc-parser");
 const ElevationAttacher = require("../igc/ElevationAttacher");
 
 function retrieveIgcFixes(folder, file) {
-  const igcAsPlainText = readIgcFile(folder, file);
+  const igcAsPlainText = Helper.readIgcFile(folder, file);
   return IGCParser.parse(igcAsPlainText, { lenient: true }).fixes;
 }
 
 test("Attach elevation data to position fixes and check values", (done) => {
+  jest.setTimeout(20_000);
+
   const fixes = retrieveIgcFixes("kai_short", "only_14_fixes.igc");
 
+  const ELEVATION_DELTA = 15;
+
   const firstFix = {
-    elevation: 250,
-    latitude: 49.95786666666667,
-    longitude: 6.994783333333333,
+    elevation: 356,
+    latitude: 50.10681666666667,
+    longitude: 7.114816666666667,
   };
 
   const lastFix = {
-    elevation: 248,
-    latitude: 49.95823333333333,
-    longitude: 6.994166666666667,
+    elevation: 355,
+    latitude: 50.10681666666667,
+    longitude: 7.114833333333333,
   };
 
   try {
     ElevationAttacher.execute(fixes, (fixesWithElevation) => {
-      expect(fixesWithElevation[0].elevation).toBe(firstFix.elevation);
+      console.log("First: ", fixesWithElevation[0]);
+      console.log("Last: ", fixesWithElevation[fixesWithElevation.length - 1]);
+
+      expect(fixesWithElevation[0].elevation).toBeGreaterThanOrEqual(
+        firstFix.elevation - ELEVATION_DELTA
+      );
+      expect(fixesWithElevation[0].elevation).toBeLessThan(
+        firstFix.elevation + ELEVATION_DELTA
+      );
       expect(fixesWithElevation[0].latitude).toBe(firstFix.latitude);
       expect(fixesWithElevation[0].longitude).toBe(firstFix.longitude);
 
-      expect(fixesWithElevation[fixesWithElevation.length - 1].elevation).toBe(
-        lastFix.elevation
-      );
+      expect(
+        fixesWithElevation[fixesWithElevation.length - 1].elevation
+      ).toBeGreaterThanOrEqual(lastFix.elevation - ELEVATION_DELTA);
+      expect(
+        fixesWithElevation[fixesWithElevation.length - 1].elevation
+      ).toBeLessThan(lastFix.elevation + ELEVATION_DELTA);
       expect(fixesWithElevation[fixesWithElevation.length - 1].latitude).toBe(
         lastFix.latitude
       );
