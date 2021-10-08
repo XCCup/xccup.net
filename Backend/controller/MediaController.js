068cf12178d46a9cad76ac745cf6230a5fb5dc92
Stephan Schöpe <socialcrap+github@posteo.de>
2021-10-08 18:26:18 +0200

    FlightImage renamed to FlightPhoto

--- Backend/controller/MediaController.js
+++ Backend/controller/MediaController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const service = require("../service/FlightImageService");
+const service = require("../service/FlightPhotoService");
 const { authToken, requesterIsNotOwner } = require("./Auth");
 const { NOT_FOUND, OK } = require("./Constants");
 const { query } = require("express-validator");
