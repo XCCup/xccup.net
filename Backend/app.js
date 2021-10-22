6579d1e8ba8af5545b39120661ae1e2fb25e782b
Kai Wissel <kai.wpkt@gmail.com>
2021-10-22 10:32:12 +0200

    Added single endpoint /media for all media files

--- Backend/app.js
+++ Backend/app.js
@@ -44,6 +44,7 @@ app.use("/results", require("./controller/ResultController"));
 app.use("/home", require("./controller/HomeController"));
 app.use("/news", require("./controller/NewsController"));
 app.use("/sponsors", require("./controller/SponsorController"));
+app.use("/media", require("./controller/MediaController"));
 
 // Handle global errors on requests. Endpoints have to forward the error to their own next() function!
 // eslint-disable-next-line no-unused-vars
---
+++ Backend/controller/MediaController.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const FlightPhoto = require("../config/postgres")["FlightPhoto"];
+const Logo = require("../config/postgres")["Logo"];
+const ProfilePicture = require("../config/postgres")["ProfilePicture"];
+const path = require("path");
+const { NOT_FOUND } = require("./Constants");
+const { query } = require("express-validator");
+const { checkParamIsUuid, validationHasErrors } = require("./Validation");
+
+// @desc Gets the flight photo
+// @route GET /media/:id
+
+router.get(
+  "/:id",
+  checkParamIsUuid("id"),
+  query("thumb").optional().isBoolean(),
+  async (req, res, next) => {
+    if (validationHasErrors(req, res)) return;
+    const id = req.params.id;
+    const thumb = req.query.thumb;
+
+    try {
+      const results = await Promise.all([
+        FlightPhoto.findByPk(id),
+        Logo.findByPk(id),
+        ProfilePicture.findByPk(id),
+      ]);
+      const media = results.find((e) => e);
+
+      if (!media) return res.sendStatus(NOT_FOUND);
+
+      const fullfilepath = thumb
+        ? path.join(path.resolve(), media.pathThumb)
+        : path.join(path.resolve(), media.path);
+
+      return res.type(media.mimetype).sendFile(fullfilepath);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// @desc Gets the meta-data to a flight photo
+// @route GET /media/meta/:id
+// TODO Is this endpoint of any interest?
+
+router.get("/meta/:id", checkParamIsUuid("id"), async (req, res, next) => {
+  if (validationHasErrors(req, res)) return;
+  const id = req.params.id;
+
+  try {
+    const media = await service.getById(id);
+
+    if (!media) return res.sendStatus(NOT_FOUND);
+
+    return res.json(media);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
