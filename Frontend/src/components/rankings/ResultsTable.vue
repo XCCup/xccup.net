bfb1132fe5f5c7ee19a65f46f2c021e55a9c65bf
Stephan Schöpe <socialcrap+github@posteo.de>
2021-10-23 11:00:55 +0200

    Ranking class fixes

--- Frontend/src/components/rankings/ResultsTable.vue
+++ Frontend/src/components/rankings/ResultsTable.vue
@@ -3,7 +3,7 @@
     <div class="card-body">
       <h6 class="card-title">
         <i class="bi bi-trophy" :class="ranking.name"></i>
-        {{ ranking.name }}
+        {{ ranking.readableName }}
       </h6>
       <p class="card-text"></p>
       <table class="table">
@@ -11,7 +11,7 @@
         <tbody>
           <tr v-for="(pilot, index) in ranking.values.slice(0, 3)" :key="index">
             <td>{{ index + 1 }}</td>
-            <td>{{ pilot.userFirstName + " " + pilot.userLastName }}</td>
+            <td>{{ pilot.user.firstName + " " + pilot.user.lastName }}</td>
             <td>
               <p class="fw-lighter">{{ pilot.totalPoints }} P</p>
             </td>
@@ -25,6 +25,9 @@
 <script>
 export default {
   name: "ResultsTable",
+  data() {
+    return {};
+  },
   props: {
     ranking: Object,
   },
