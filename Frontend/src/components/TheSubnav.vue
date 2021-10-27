cdc707ec765f9c3eba8361a8a4e8b16677686b73
Stephan Schöpe <socialcrap+github@posteo.de>
2021-10-27 19:05:07 +0200

    Link back no works as expected

--- Frontend/src/components/TheSubnav.vue
+++ Frontend/src/components/TheSubnav.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container-fluid flight-info text-light mb-0 p-1">
     <p class="m-0">
-      <router-link to="/"><i class="bi bi-chevron-left mx-2"></i> </router-link>
+      <a href="#" @click="$router.go(-1)"
+        ><i class="bi bi-chevron-left mx-2"></i
+      ></a>
 
       Flug von
       <a href="#">{{ flight.User.firstName + " " + flight.User.lastName }}</a>
