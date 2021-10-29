85bf2b1bd436aa4b510800b5a6600a3b2484321f
Kai Wissel <kai.wpkt@gmail.com>
2021-10-29 13:00:33 +0200

    Added further data options (countries,states,gender,...) to /general

--- Backend/controller/GeneralController.js
+++ Backend/controller/GeneralController.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getCurrentActive } = require("../service/SeasonService");
+const { getCurrentActive } = require("../service/generalervice");
 const { getAllBrands } = require("../service/FlightService");
+const { STATE: flightStates, TYPE } = require("../constants/flight-constants");
+const {
+  GENDER,
+  COUNTRY,
+  STATE,
+  TSHIRT_SIZES,
+} = require("../constants/user-constants");
 
 // @desc Gets all gliderClasses of the current season
-// @route GET /seasons
+// @route GET /general/gliderClasses
 
 router.get("/gliderClasses", async (req, res, next) => {
   try {
@@ -16,7 +23,7 @@ router.get("/gliderClasses", async (req, res, next) => {
 });
 
 // @desc Gets all rankingClasses of the current season
-// @route GET /seasons
+// @route GET /general/rankingClasses
 
 router.get("/rankingClasses", async (req, res, next) => {
   try {
@@ -28,12 +35,78 @@ router.get("/rankingClasses", async (req, res, next) => {
 });
 
 // @desc Gets all brands of gliders which exist in the db
-// @route GET /seasons
+// @route GET /general/brands
 
 router.get("/brands", async (req, res, next) => {
   try {
     const brands = await getAllBrands();
     res.json(brands);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc Gets all possiblites of flightStates
+// @route GET /general/flight/states
+
+router.get("/flight/states", async (req, res, next) => {
+  try {
+    res.json(flightStates);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc Gets all possiblites of flightTypes
+// @route GET /general/flight/types
+
+router.get("/flight/types", async (req, res, next) => {
+  try {
+    res.json(TYPE);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc Gets all possiblites of genders
+// @route GET /general/user/genders
+
+router.get("/user/genders", async (req, res, next) => {
+  try {
+    res.json(GENDER);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc Gets all by the xccup regconized countries
+// @route GET /general/user/countries
+
+router.get("/user/countries", async (req, res, next) => {
+  try {
+    res.json(COUNTRY);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc Gets all by the xccup regconized states of countries
+// @route GET /general/user/states
+
+router.get("/user/states", async (req, res, next) => {
+  try {
+    res.json(STATE);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc Gets all possible tshirt sizes
+// @route GET /general/user/tshirtSizes
+
+router.get("/user/tshirtSizes", async (req, res, next) => {
+  try {
+    res.json(TSHIRT_SIZES);
   } catch (error) {
     next(error);
   }
