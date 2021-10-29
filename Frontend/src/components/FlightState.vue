c6ae5b9dcdbad38f08cc98615ec712e04ca6a207
Stephan Schöpe <socialcrap+github@posteo.de>
2021-10-29 18:11:28 +0200

    New flight states added

--- Frontend/src/components/FlightState.vue
+++ Frontend/src/components/FlightState.vue
@@ -4,9 +4,13 @@
     v-if="flightState === `Nicht in Wertung`"
     class="bi bi-x-circle text-danger"
   ></i>
-  <!-- <i class="bi bi-x-circle"></i>
-  <i class="bi bi-exclamation-circle"></i>
-  <i class="bi bi-exclamation-triangle"></i> -->
+  <i v-if="flightState === `Flugbuch`" class="bi bi-journal-text"></i>
+  <i v-if="flightState === `In Bearbeitung`" class="bi bi-hourglass-split"></i>
+
+  <!-- <i class="bi bi-x-circle"></i> -->
+  <!-- <i class="bi bi-exclamation-circle"></i> -->
+  <!-- <i class="bi bi-exclamation-triangle"></i> -->
+  -->
 </template>
 
 <script>
