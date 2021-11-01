f68bccd0f06103447fb89fad957d96b4e4c24ad6
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-01 09:54:25 +0100

    Newcomer ranking draft

---
+++ Frontend/src/views/RankingNewcomer.vue
@@ -0,0 +1,24 @@
+<template>Newcomer Ranking</template>
+
+<script>
+import ApiService from "@/services/ApiService.js";
+// import FlightsTable from "@/components/rankings/FlightsTable";
+
+import { ref } from "vue";
+
+export default {
+  name: "RankingNewcomer",
+  components: {},
+
+  async setup() {
+    try {
+      const { data: initialData } = await ApiService.getNewcomerRanking();
+      return {
+        flights: ref(initialData),
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
