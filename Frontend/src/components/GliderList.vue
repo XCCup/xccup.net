dcf34ac6893933585666e3193ff358c7c15bc35d
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-01 23:43:48 +0100

    First draft of GliderList

---
+++ Frontend/src/components/GliderList.vue
@@ -0,0 +1,53 @@
+<template>
+  <h5>Standard Gerät wählen</h5>
+  <!-- <ul class="list-group">
+    <li v-for="glider in gliders" :key="glider.id" class="list-group-item">
+      {{ formatGlider(glider) }}
+      <i class="bi bi-pencil-square mx-1"></i>
+      <i class="bi bi-trash"></i>
+    </li>
+  </ul> -->
+
+  <div v-for="glider in gliders" :key="glider.id" class="form-check mt-2">
+    <input
+      class="form-check-input"
+      type="radio"
+      name="exampleRadios"
+      :id="glider.id"
+      value="option1"
+      checked
+    />
+    <label class="form-check-label" :for="glider.id">
+      {{ formatGlider(glider) }}
+      <i class="bi bi-pencil-square mx-1"></i>
+      <i class="bi bi-trash"></i
+    ></label>
+  </div>
+  <button type="button" class="btn btn-primary mt-2 me-2">Speichern</button>
+  <button
+    type="button"
+    class="btn btn-outline-primary mt-2"
+    data-bs-toggle="modal"
+    data-bs-target="#addGliderModal"
+  >
+    <i class="bi bi-plus"></i> Gerät hinzufügen
+  </button>
+</template>
+
+<script>
+export default {
+  name: "GliderSelect",
+
+  props: {
+    gliders: {
+      type: Array,
+    },
+  },
+
+  setup() {
+    const formatGlider = (glider) =>
+      glider.brand + " " + glider.model + " (" + glider.shortDescription + ")";
+    return { formatGlider };
+  },
+};
+</script>
--- Frontend/src/views/Profile.vue
+++ Frontend/src/views/Profile.vue
@@ -10,14 +10,15 @@
               class="rounded-circle"
               width="150px"
               src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-            /><span class="font-weight-bold">Lorem Ipsum</span
-            ><span class="text-black-50">Lorem, ipsum.</span>
+            />
+            <span class="font-weight-bold"> Foo </span>
+            <span class="text-black-50">Bar</span>
           </div>
         </div>
 
         <!-- Center -->
         <div class="col-md-9">
-          <div class="p-3">
+          <div v-if="editMode" class="p-3">
             <div class="d-flex justify-content-between align-items-center mb-3">
               <h4 class="text-right">Profil</h4>
             </div>
@@ -51,6 +52,161 @@
                       v-model="userProfile.address.city"
                       label="Stadt"
                     />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <BaseInput
+                      v-model="userProfile.address.state"
+                      label="Bundesland"
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <BaseInput
+                      v-model="userProfile.address.country"
+                      label="Land"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <BaseSelect
+                      v-model="userProfile.gender"
+                      label="Geschlecht"
+                      :showLabel="true"
+                      :options="['M', 'W', 'D']"
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <BaseSelect
+                      v-model="userProfile.tshirtSize"
+                      label="T-Shirt Größe"
+                      :showLabel="true"
+                      :options="['S', 'M', 'L', 'XL', 'XXL']"
+                    />
+                  </div>
+                </div>
+
+                <div class="mt-3"></div>
+                <hr />
+              </div>
+              <!-- Glider select -->
+              <div class="col-md-12">
+                <div class="row d-flex align-items-end">
+                  <div class="col-md-7">
+                    <div class="">
+                      <!-- <GliderSelect
+                        v-model="userProfile.defaultGlider"
+                        :showLabel="true"
+                        label="Standard Gerät"
+                        :gliders="userProfile.gliders"
+                        :isDisabled="false"
+                      /> -->
+                    </div>
+                  </div>
+                  <div class="col-md-5">
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target="#addGliderModal"
+                      >
+                        Bearbeiten
+                      </button>
+                      <!-- <button
+                        type="button"
+                        class="btn btn-outline-danger"
+                        data-bs-toggle="modal"
+                        data-bs-target="#removeGliderModal"
+                      >
+                        Entfernen
+                      </button> -->
+                    </div>
+                  </div>
+                </div>
+                <GliderList :gliders="userProfile.gliders" />
+              </div>
+            </div>
+            <hr />
+            <h5>Benachrichtigungen</h5>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="notifyForComment"
+                v-model="userProfile.emailInformIfComment"
+              />
+              <label class="form-check-label" for="flexCheckDefault">
+                Email bei neuem Kommentar <i class="bi bi-info-circle"></i>
+              </label>
+            </div>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="optInNewsletter"
+                v-model="userProfile.emailNewsletter"
+              />
+              <label class="form-check-label" for="flexCheckDefault">
+                Newsletter abonnieren <i class="bi bi-info-circle"></i>
+              </label>
+            </div>
+
+            <br />
+            <button
+              class="btn btn-primary"
+              :disabled="!profileDataHasChanged"
+              @click="save"
+            >
+              Speichern
+              <div
+                v-if="showSpinner"
+                class="spinner-border spinner-border-sm"
+                role="status"
+              >
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </button>
+
+            <!-- Edit -->
+            <!-- <div v-if="!edit">
+              <router-link
+                :to="{ name: 'ProfileEdit' }"
+                class="btn btn-primary"
+              >
+                Edit
+              </router-link>
+            </div>
+            <div v-if="edit">
+              <div>Edit: {{ userProfile.firstName }}</div>
+
+              <button class="btn btn-primary" @click="save">Speichern</button>
+              <button class="btn btn-outline-danger" @click="cancel">
+                Abbrechen
+              </button>
+            </div> -->
+            <!-- Edit -->
+          </div>
+          <div v-if="!editMode" class="p-3">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <h4 class="text-right">Profil</h4>
+            </div>
+
+            <div class="row mt-3">
+              <div class="col-md-12">
+                <p>{{ userProfile.birthday }}</p>
+                <p>{{ userProfile.email }}</p>
+                <p>{{ userProfile.address.street }}</p>
+
+                <div class="row">
+                  <div class="col-md-6">
+                    {{ userProfile.address.zip }}
+                  </div>
+                  <div class="col-md-6">
+                    {{ userProfile.address.city }}
                   </div>
                 </div>
                 <div class="row">
@@ -188,39 +344,6 @@
             <!-- Edit -->
           </div>
         </div>
-        <!-- Right -->
-        <!-- <div class="col-md-4">
-          <div class="p-3 py-5">
-            <div
-              class="d-flex justify-content-between align-items-center experience"
-            >
-              <span>Edit Experience</span
-              ><span class="border px-3 p-1 add-experience"
-                ><i class="fa fa-plus"></i>&nbsp;Experience</span
-              >
-            </div>
-            <br />
-            <div class="col-md-12">
-              <label class="labels">Experience in Designing</label
-              ><input
-                type="text"
-                class="form-control"
-                placeholder="experience"
-                value=""
-              />
-            </div>
-            <br />
-            <div class="col-md-12">
-              <label class="labels">Additional Details</label
-              ><input
-                type="text"
-                class="form-control"
-                placeholder="additional details"
-                value=""
-              />
-            </div>
-          </div>
-        </div> -->
       </div>
     </div>
   </div>
@@ -236,10 +359,11 @@
 <script>
 import ApiService from "@/services/ApiService.js";
 
-import { mapGetters, useStore } from "vuex";
-import { /*computed,*/ ref } from "vue";
+import { mapGetters } from "vuex";
+import { ref } from "vue";
 
 import GliderSelect from "@/components/GliderSelect";
+import GliderList from "@/components/GliderList";
 
 import ModalAddGlider from "@/components/ModalAddGlider";
 import ModalRemoveGlider from "@/components/ModalRemoveGlider";
@@ -247,7 +371,7 @@ import cloneDeep from "lodash/cloneDeep";
 
 export default {
   name: "Profile",
-  components: { ModalAddGlider, ModalRemoveGlider, GliderSelect },
+  components: { ModalAddGlider, ModalRemoveGlider, GliderSelect, GliderList },
   async setup() {
     // TODO: Remove if store will not be used for user details. Maybe leave it here for reference how to do it;)
     // const store = useStore();
@@ -267,6 +391,7 @@ export default {
       userProfile: null,
       unmodifiedUserProfile: null,
       showSpinner: false,
+      editMode: true,
     };
   },
   computed: {
