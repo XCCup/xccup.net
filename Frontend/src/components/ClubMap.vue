b1700a6b1748b120e65599baa98331cd547674b6
KaiWissel <kai.wpkt@gmail.com>
2021-11-12 15:29:02 +0100

    Finalized club map

---
+++ Frontend/src/components/ClubMap.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="mapContainer"></div>
+</template>
+
+<script setup>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { GestureHandling } from "leaflet-gesture-handling";
+import "leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
+import tileOptions from "@/config/mapbox.js";
+import { ref, onMounted } from "vue";
+
+const map = ref(null)
+const logos = ref([])
+
+const props = defineProps({
+  clubs: {
+    type: Array,
+    default: []
+  },
+})
+
+const createPopupContent = (club) => {
+  const baseUrl = import.meta.env.VITE_API_URL;
+
+  const lines = [];
+  lines.push(`<strong>${club.name}</strong>`);
+  lines.push(`Anzahl Teilnahmen seit 2011: ${club.participantInSeasons.length}`);
+  // It was also considered to add the total number of participants. But due to concerns that some clubs will stay absent when there are only a few participants, this idea is for now postponed.
+  // lines.push(`Mitglieder im XCCup: 42`);
+  lines.push(`<a>${club.website}</a>`);
+  lines.push(`<img class="icon-logo" style="background: rgba(255,255,255,.5);" src="${baseUrl}clubs/logo/${club.logo.id}?thumb=true" height="50" max-width="150">`);
+
+  return lines.join("<br>")
+}
+
+const addClubLogos = (clubs) => {
+  if (clubs.length === 0) return;
+
+  const logoGroup = new L.featureGroup();
+  const logoBackgroundGroup = new L.featureGroup();
+
+  clubs.forEach((club) => {
+    if (!club.mapPosition) return
+
+    logos.value.push(L.marker([club.mapPosition.lat, club.mapPosition.lon], { title: club.name, riseOnHover: true }).bindTooltip(club.name,
+      {
+        permanent: true,
+        direction: 'right'
+      }).bindPopup(createPopupContent(club)).addTo(logoGroup))
+  });
+
+  map.value.addLayer(logoGroup);
+  map.value.fitBounds(logoGroup.getBounds());
+};
+
+onMounted(() => {
+  L.Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
+
+  map.value = L.map("mapContainer", {
+    gestureHandling: true,
+  }).setView([50.143, 7.146], 8)
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}",
+    tileOptions
+  ).addTo(map.value);
+
+  addClubLogos(props.clubs);
+})
+
+</script>
+
+<style scoped>
+#mapContainer {
+  height: 100%;
+  min-height: 800px;
+}
+</style>
--- Frontend/src/views/Clubs.vue
+++ Frontend/src/views/Clubs.vue
@@ -2,24 +2,58 @@
     <div class="container-fluid">
         <h3>Teilnehmende Vereine des Jahres {{ new Date().getFullYear() }}</h3>
     </div>
-    <div class="card" v-for="(club, index) in clubs" v-bind:key="club.id">
-        <div>ID: {{ club.id }}</div>
-        <Sponsor
-            :sponsor="club"
-            :linkMessage="linkMessages[Math.floor(Math.random() * linkMessages.length)]"
-        />
+    <div class="row">
+        <!-- Table looks so "2000". We will with just use a map. -->
+        <!-- <div class="col-xl-5 col-md-6 col-12">
+            <div class="text-light p-4 pb-4">
+                <table class="table table-striped table-hover text-sm">
+                    <thead>
+                        <th>Verein</th>
+                        <th>Anzahl Teilnahmen seit 2011</th>
+                        <th>Mitglieder im XCCup</th>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="(club, index) in clubs"
+                            v-bind:item="club"
+                            v-bind:index="index"
+                            v-bind:key="club.id"
+                            @click="openClubWebsite(club.website)"
+                        >
+                            <td>{{ club.name }}</td>
+                            <td>{{ club.participantInSeasons.length }}</td>
+                            <td>{{ 42 }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>-->
+        <div class="col">
+            <ClubMap :highlightedClubId="highlightedClubId" :clubs="clubs" />
+        </div>
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+// import { useRouter } from "vue-router";
 import ApiService from "@/services/ApiService";
 import { shuffle } from "lodash"
 
+// const router = useRouter();
 const responseData = shuffle((await ApiService.getClubs()).data);
 
-const linkMessages = ref(["Hit it!", "Check it out", "Klick mich", "Show more", "Erfahre mehr", "Besuche uns", "Visit us"])
 const clubs = ref(responseData)
 
+// const openClubWebsite = (url) => {
+//     window.open(url);
+// }
+
 </script>
+
+<style scoped>
+tr:hover {
+    cursor: pointer;
+}
+</style>
 
