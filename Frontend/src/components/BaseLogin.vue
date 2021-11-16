b3a6f792f1415cfb67ed6d3d5da93a1554d6f31d
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-16 22:15:45 +0100

    Login error handling and logic improvements

--- Frontend/src/components/BaseLogin.vue
+++ Frontend/src/components/BaseLogin.vue
@@ -1,40 +1,38 @@
 <template>
-  <form class="px-4 py-3" @submit.prevent="handleSubmit">
-    <div class="mb-3">
-      <label for="email" class="form-label">E-Mail</label>
-      <input
-        id="email"
-        v-model="email"
-        type="email"
-        class="form-control"
-        placeholder="E-Mail"
-      />
-    </div>
-    <div class="mb-3">
-      <label for="password" class="form-label">Passwort</label>
-      <input
-        id="password"
-        v-model="password"
-        type="password"
-        class="form-control"
-        placeholder="Passwort"
-      />
-    </div>
-    <!-- Stay logged in not realy useful with JWT auth -->
-    <!-- <div class="mb-3">
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="dropdownCheck" />
-        <label class="form-check-label" for="dropdownCheck">
-          Angemeldet bleiben
-        </label>
+  <div class="col-6 mx-auto mt-2">
+    <form @submit.prevent="handleSubmit">
+      <div class="mb-3">
+        <label for="email" class="form-label">E-Mail</label>
+        <input
+          id="email"
+          v-model="email"
+          type="email"
+          class="form-control"
+          placeholder="E-Mail"
+        />
       </div>
-    </div> -->
-    <button type="submit" class="btn btn-primary">Anmelden</button>
-  </form>
-  <!-- TODO: Dynamic classes depending on where this component is used? -->
-  <div class="dropdown-divider"></div>
-  <a class="dropdown-item" href="#">Registrieren</a>
-  <a class="dropdown-item" href="#">Password vergessen?</a>
+      <div class="mb-3">
+        <label for="password" class="form-label">Passwort</label>
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          class="form-control"
+          placeholder="Passwort"
+        />
+      </div>
+      <div v-if="errorMessage" id="loginErrorMessage" class="my-2 text-danger">
+        {{ errorMessage }}
+      </div>
+      <button type="submit" class="btn btn-primary">Anmelden</button>
+    </form>
+
+    <!-- TODO: -->
+    <div class="mt-3">
+      <p><a href="#">Password vergessen?</a></p>
+      <p><a href="#">Registrieren</a></p>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -44,14 +42,15 @@ import { ref } from "vue";
 const { login } = useUser();
 const router = useRouter();
 
-defineProps({
-  redirectAfterLogin: {
-    type: Boolean,
-    default: true,
-  },
-});
+// defineProps({
+//   redirectAfterLogin: {
+//     type: Boolean,
+//     default: true,
+//   },
+// });
 const email = ref("");
 const password = ref("");
+const errorMessage = ref("");
 
 const handleSubmit = async () => {
   try {
@@ -65,11 +64,21 @@ const handleSubmit = async () => {
       let searchParams = new URLSearchParams(window.location.search);
       if (searchParams.has("redirect")) {
         router.push({ path: `${searchParams.get("redirect")}` });
+      } else {
+        router.go(-1);
       }
     }
   } catch (error) {
-    // TODO: Display error message
-    console.log(error);
+    if (error.response.status === 401) {
+      errorMessage.value = "Benutzername/Passwort falsch";
+    } else if (error.response.status === 429) {
+      errorMessage.value =
+        "Zu viele Versuche, bitte versuche es später noch mal.";
+    } else {
+      errorMessage.value =
+        "Da ist was schief gegangen, bitte versuche es später noch mal.";
+      console.log(error.response);
+    }
   }
 };
 </script>
