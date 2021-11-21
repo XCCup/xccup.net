8cb57f0b73540f247c62a9b09dfc6a26748e7544
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-21 11:34:17 +0100

    api-rate-limit bugfix

--- Backend/controller/api-protection.js
+++ Backend/controller/api-protection.js
@@ -2,12 +2,9 @@ const rateLimit = require("express-rate-limit");
 
 function createLimiter(windowMinutes, maxRequestsInWindow) {
   return rateLimit({
-    windowMs: process.env.DISABLE_API_PROTECTION
-      ? Number.MAX_SAFE_INTEGER
-      : windowMinutes * 60 * 1000,
-    max: process.env.DISABLE_API_PROTECTION
-      ? Number.MAX_SAFE_INTEGER
-      : maxRequestsInWindow,
+    windowMs: windowMinutes * 60 * 1000,
+    max:
+      process.env.DISABLE_API_PROTECTION === "true" ? 0 : maxRequestsInWindow,
   });
 }
 
