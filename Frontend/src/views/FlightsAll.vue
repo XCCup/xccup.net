8bd452e70679f5c429c8cebf960c4947e653ef10
KaiWissel <kai.wpkt@gmail.com>
2021-11-24 22:39:34 +0100

    Final draft flight filter dialog

--- Frontend/src/views/FlightsAll.vue
+++ Frontend/src/views/FlightsAll.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid mb-3">
     <h3>Streckenmeldungen {{ computedYear }}</h3>
+    <button
+      type="button"
+      class="btn btn-outline-primary btn-sm m-1 col-4 col-sm-2 col-lg-1"
+      @click="onFilter"
+    >
+      Filter <i v-if="filterActive" class="bi bi-funnel"></i>
+    </button>
   </div>
   <FlightsTable :flights="flights" @table-sort-changed="handleSortChange" />
+  <ModalFilterFlights @filter-changed="handleFilterChange" />
 </template>
 
 <script setup>
 import ApiService from "@/services/ApiService.js";
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { setWindowName } from "../helper/utils";
+import { Modal } from "bootstrap";
 
 const route = useRoute();
 
@@ -20,24 +29,52 @@ const props = defineProps({
   },
 });
 
+const flights = ref(null);
+
+let filterModal;
+
+let sortOptions;
+let filterOptions;
+
+const filterActive = ref(false);
+
 const computedYear = computed(() => (props.year.length ? props.year : ""));
+
+onMounted(() => {
+  filterModal = new Modal(document.getElementById("flightFilterModal"));
+});
+await retrieveFlights();
 
 setWindowName("Streckenmeldungen");
 
-const flights = ref(null);
-await retrieveFlights();
-
-const handleSortChange = (value) => {
-  console.log("FlightsAll: ", value);
-  retrieveFlights(value);
+const onFilter = () => {
+  filterModal.show();
 };
 
-async function retrieveFlights(sortObject) {
+const handleSortChange = (value) => {
+  sortOptions = value;
+  retrieveFlights();
+};
+
+const handleFilterChange = (filterIds) => {
+  filterOptions = filterIds;
+  filterModal.hide();
+  calcFilterActive();
+  retrieveFlights();
+};
+
+function calcFilterActive() {
+  filterActive.value =
+    filterOptions && Object.values(filterOptions).find((v) => !!v);
+}
+
+async function retrieveFlights() {
   try {
     const { data: initialData } = await ApiService.getFlights({
       ...route.params,
-      sortCol: sortObject?.sortCol,
-      sortOrder: sortObject?.sortOrder,
+      sortCol: sortOptions?.sortCol,
+      sortOrder: sortOptions?.sortOrder,
+      ...filterOptions,
     });
     flights.value = initialData;
   } catch (error) {
