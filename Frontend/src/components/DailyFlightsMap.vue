90aa464ba4946d53e4696a8aa375f060f149376e
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-25 22:14:45 +0100

    webpack fix undone /bc not working

--- Frontend/src/components/DailyFlightsMap.vue
+++ Frontend/src/components/DailyFlightsMap.vue
@@ -46,13 +46,13 @@ export default {
     ).addTo(this.map);
 
     // Webpack fix for default marker image paths
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.imagePath = "/";
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-      iconUrl: require("leaflet/dist/images/marker-icon.png"),
-      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-    });
+    // delete L.Icon.Default.prototype._getIconUrl;
+    // L.Icon.Default.imagePath = "/";
+    // L.Icon.Default.mergeOptions({
+    //   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    //   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    //   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    // });
 
     // Draw tracklogs
     this.drawTracks(this.tracks);
--- Frontend/src/components/FlightMap.vue
+++ Frontend/src/components/FlightMap.vue
@@ -61,13 +61,13 @@ onMounted(() => {
   ).addTo(map.value);
 
   // Webpack fix for default marker image paths
-  delete L.Icon.Default.prototype._getIconUrl;
-  L.Icon.Default.imagePath = "/";
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-    iconUrl: require("leaflet/dist/images/marker-icon.png"),
-    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-  });
+  // delete L.Icon.Default.prototype._getIconUrl;
+  // L.Icon.Default.imagePath = "/";
+  // L.Icon.Default.mergeOptions({
+  //   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  //   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  //   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  // });
 
   // Draw tracklogs and Airspaces
   drawTracks(tracklogs.value);
