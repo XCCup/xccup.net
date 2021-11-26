36ee760a48dcbc5a2c03c97db12c8674290c2c5e
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-26 16:39:14 +0100

    bugfix

--- Frontend/src/helper/base-url-helper.js
+++ Frontend/src/helper/base-url-helper.js
@@ -5,7 +5,7 @@ export function getbaseURL() {
     window.location.hostname.toString() +
     ":3000/api/";
   if (process.env.NODE_ENV == "production") {
-    import.meta.env.VITE_API_URL;
+    URL = import.meta.env.VITE_API_URL;
   }
   return URL;
 }
