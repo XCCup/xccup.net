f8d5bb87b6f1b5d5c9e350166c32262d9090810e
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-26 19:47:53 +0100

    refactor

--- Frontend/src/helper/base-url-helper.js
+++ Frontend/src/helper/base-url-helper.js
@@ -1,11 +1,5 @@
 export function getbaseURL() {
-  let URL =
-    window.location.protocol.toString() +
-    "//" +
-    window.location.hostname.toString() +
-    ":3000/api/";
-  if (process.env.NODE_ENV == "production") {
-    URL = import.meta.env.VITE_API_URL;
-  }
-  return URL;
+  if (process.env.NODE_ENV == "production") return import.meta.env.VITE_API_URL;
+
+  return `${location.protocol}//${location.hostname}:3000/api/`;
 }
