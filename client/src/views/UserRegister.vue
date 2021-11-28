23e984d87e794157df1ffbdc1af4f8f80b284e00
KaiWissel <kai.wpkt@gmail.com>
2021-11-28 10:59:27 +0100

    Revert "user register bugfix"
    This reverts commit c8ea64529cf32c8a3e98160312b4872cbada7311.
    
    Revert last commit

--- client/src/views/UserRegister.vue
+++ client/src/views/UserRegister.vue
@@ -177,9 +177,14 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref, computed } from "vue";
 import ApiService from "@/services/ApiService";
 import { setWindowName } from "../helper/utils";
+import { useRouter } from "vue-router";
+import useUserData from "@/composables/useUserSignup";
+
+const { userData } = useUserData();
+const router = useRouter();
 
 setWindowName("Anmelden");
 
@@ -213,6 +218,7 @@ const userData = reactive({
 const limitDate = new Date();
 limitDate.setYear(limitDate.getFullYear() - 16);
 const upperLimitBirthday = ref(limitDate);
+
 
 // Helpers
 const showSpinner = ref(false);
