3fd826e3a7ccb41a5340cc80c74dd28f2f99d16e
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-28 17:46:31 +0100

    conflict management

--- client/src/views/UserActivate.vue
+++ client/src/views/UserActivate.vue
@@ -1,9 +1,13 @@
 <template>
-  <!-- TODO: Put this in a dialog as well -->
   <div class="container mb-3">
     <h3>Nutzerprofilaktivierung</h3>
     <div v-if="state == 'incompleted'">
       Es wurde kein Konto zur Aktivierung gefunden 🤨
+    </div>
+    <div v-if="state == 'success'">
+      <h5>Hallo {{ firstName }}!</h5>
+      <div>Deine Aktivierung war erfolgreich 🥳</div>
+      <div>Du solltest nun automatisch eingeloggt sein</div>
     </div>
     <div v-if="state == 'fail'">
       <div>Es gab leider ein Problem mit der Aktivierung 😥</div>
@@ -17,33 +21,26 @@
 
 <script setup>
 import { ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { useRoute } from "vue-router";
 import ApiService from "../services/ApiService";
-import useUser from "@/composables/useUser";
-const { saveTokenData } = useUser();
 
-const route = useRoute();
-const router = useRouter();
-
-const { userId, token } = route.query;
+const router = useRoute();
+const { userId, token } = router.query;
 
 const state = ref("");
+const firstName = ref("");
 
 if (!(userId && token)) {
   state.value = "incompleted";
 } else {
   try {
     const res = await ApiService.activate(userId, token);
-    if (res.status != 200 && res.status != 404) throw res.statusText;
+    if (res.status != 200) throw res.statusText;
 
-    if (res.status == 200) {
-      saveTokenData(res.data);
-      router.push({ name: "Home" });
-    }
+    state.value = "success";
+    firstName.value = res.data.firstName;
 
-    if (res.status == 404) {
-      state.value = "fail";
-    }
+    //TODO: Handle token
   } catch (error) {
     state.value = "fail";
     console.error(error);
