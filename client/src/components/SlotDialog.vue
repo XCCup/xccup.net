b4d61e3c9fabb7e000d3a8c27569309303ea9bec
Stephan Schöpe <socialcrap+github@posteo.de>
2021-11-29 14:10:09 +0100

    first draft of dialog slot

---
+++ client/src/components/SlotDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="bg-light dialog-bg-image">
+    <div class="bg-gradient-1">
+      <div class="container py-5 h-100">
+        <div class="row justify-content-center align-items-center h-100">
+          <div v-if="!signupSuccessfull" class="col-12 col-lg-9 col-xl-7">
+            <div class="card shadow-2-strong" style="border-radius: 15px">
+              <div class="card-body p-4 p-md-5">
+                <slot></slot>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
