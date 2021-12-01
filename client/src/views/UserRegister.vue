059273701275042541a502f55b9523c44e7a4c67
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-01 18:22:37 +0100

    id bugfix

--- client/src/views/UserRegister.vue
+++ client/src/views/UserRegister.vue
@@ -102,7 +102,7 @@
         <div class="row">
           <div class="col-md-6 mb-3">
             <BaseSelect
-              id="country"
+              id="state"
               v-model="userData.address.state"
               label="Bundesland"
               :show-label="true"
