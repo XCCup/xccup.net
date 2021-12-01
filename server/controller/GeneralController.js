fa9b65eb78e3b59f003592fe929986cf0f961517
Kai Wissel <kai.wpkt@gmail.com>
2021-12-01 21:41:56 +0100

    Removed unused endpoints

--- server/controller/GeneralController.js
+++ server/controller/GeneralController.js
@@ -88,48 +88,4 @@ router.get("/user/constants", async (req, res, next) => {
   }
 });
 
-// @desc Gets all possiblites of genders
-// @route GET /general/user/genders
-
-router.get("/user/genders", async (req, res, next) => {
-  try {
-    res.json(genders);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// @desc Gets all by the xccup regconized countries
-// @route GET /general/user/countries
-
-router.get("/user/countries", async (req, res, next) => {
-  try {
-    res.json(countries);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// @desc Gets all by the xccup regconized states of countries
-// @route GET /general/user/states
-
-router.get("/user/states", async (req, res, next) => {
-  try {
-    res.json(conutryStates);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// @desc Gets all possible tshirt sizes
-// @route GET /general/user/tshirtSizes
-
-router.get("/user/tshirtSizes", async (req, res, next) => {
-  try {
-    res.json(TSHIRT_SIZES);
-  } catch (error) {
-    next(error);
-  }
-});
-
 module.exports = router;
