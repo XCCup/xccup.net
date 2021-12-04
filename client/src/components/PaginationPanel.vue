cd64a7b3ab8d16f838a1c4143628f8d0b7dbb482
KaiWissel <kai.wpkt@gmail.com>
2021-12-04 20:03:04 +0100

    Final draft for pagination

--- client/src/components/PaginationPanel.vue
+++ client/src/components/PaginationPanel.vue
@@ -1,10 +1,10 @@
 <template>
   <nav aria-label="Flights pagination">
-    <ul class="pagination pagination-sm">
-      <li class="page-item">
+    <ul class="pagination pagination-sm justify-content-end align-items-center">
+      <li class="page-item me-2">
         <select
           v-model="numberFlightsPerPage"
-          class="form-select form-select-sm"
+          class="form-select form-select-sm hide-on-xs border-primary"
         >
           <option disabled value="" selected>Anzahl Flüge pro Seite</option>
           <option v-for="option in LIMIT_OPTIONS" :key="option" :value="option">
@@ -12,8 +12,9 @@
           </option>
         </select>
       </li>
-      <li class="page-item">
-        {{ currentRange.start }}-{{ currentRange.end }} von
+      <li class="page-item me-2">
+        {{ currentRange.start }}-{{ currentRange.end }}
+        von
         {{ numberOfTotalFlights }}
       </li>
       <li
@@ -21,7 +22,7 @@
         :class="disableIfNoPreviousEntriesAvailable"
         @click="onFirst"
       >
-        <a class="page-link" href="#" aria-label="First">
+        <a class="page-link border-primary" href="#" aria-label="First">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -30,7 +31,7 @@
         :class="disableIfNoPreviousEntriesAvailable"
         @click="onPrevious"
       >
-        <a class="page-link" href="#" aria-label="Previous">
+        <a class="page-link border-primary" href="#" aria-label="Previous">
           <span aria-hidden="true">&lsaquo;</span>
         </a>
       </li>
@@ -39,7 +40,12 @@
         :class="disableIfNoNextEntriesAvailable"
         @click="onNext"
       >
-        <a class="page-link" :disabled="true" href="#" aria-label="Next">
+        <a
+          class="page-link border-primary"
+          :disabled="true"
+          href="#"
+          aria-label="Next"
+        >
           <span aria-hidden="true">&rsaquo;</span>
         </a>
       </li>
@@ -48,7 +54,7 @@
         :class="disableIfNoNextEntriesAvailable"
         @click="onLast"
       >
-        <a class="page-link" href="#" aria-label="Last">
+        <a class="page-link border-primary" href="#" aria-label="Last">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
@@ -72,7 +78,6 @@ const numberFlightsPerPage = ref(DEFAULT_LIMIT);
 
 watch(numberFlightsPerPage, () => {
   //Don't use watchEffect because this will always run once on setup and cause a second unneccessary request
-  console.log(numberFlightsPerPage.value);
   fetchFlights({
     limit: numberFlightsPerPage.value,
     offset: currentRange.value.start - 1,
@@ -88,22 +93,25 @@ const disableIfNoNextEntriesAvailable = computed(() => {
 });
 
 const onFirst = () => {
-  console.log("Clicked first ", numberFlightsPerPage.value);
+  if (disableIfNoPreviousEntriesAvailable.value) return;
   fetchFlights({ limit: numberFlightsPerPage.value, offset: 0 });
 };
 const onPrevious = () => {
+  if (disableIfNoPreviousEntriesAvailable.value) return;
   fetchFlights({
     limit: numberFlightsPerPage.value,
     offset: currentRange.value.start - numberFlightsPerPage.value - 1,
   });
 };
 const onNext = () => {
+  if (disableIfNoNextEntriesAvailable.value) return;
   fetchFlights({
     limit: numberFlightsPerPage.value,
     offset: currentRange.value.end,
   });
 };
 const onLast = () => {
+  if (disableIfNoNextEntriesAvailable.value) return;
   fetchFlights({
     limit: numberFlightsPerPage.value,
     offset: numberOfTotalFlights.value - numberFlightsPerPage.value,
