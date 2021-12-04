7d9b9b96da94b117c59d0b55b39496c42e626168
KaiWissel <kai.wpkt@gmail.com>
2021-12-04 09:39:05 +0100

    G-Check Error will be shown in dialog

--- client/src/components/UploadForm.vue
+++ client/src/components/UploadForm.vue
@@ -316,6 +316,12 @@ const igcSelected = async (file) => {
     detailsCollapse.show();
   } catch (error) {
     detailsCollapse.hide();
+    console.log(error.response);
+    if (
+      error.response.status === 400 &&
+      error.response.data == "Invalid G-Record"
+    )
+      return (errorMessage.value = `Dieser Flug resultiert gem. FAI in einem negativen G-Check (http://vali.fai-civl.org/validation.html). Bitte prüfe ob die Datei unverändert ist. Wenn du denkst dass dies ein Fehler ist wende dich bitte an ${Constants.ADMIN_EMAIL}`);
     if (error.response.status === 403)
       // TODO: Find a way to make the email clickable without using v-html
       return (errorMessage.value = `Dieser Flug liegt ausserhalb des XCCup Gebiets. Wenn du denkst dass dies ein Fehler ist wende dich bitte an ${Constants.ADMIN_EMAIL}`);
