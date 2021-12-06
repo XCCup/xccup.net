e8264e6a822199dd59d16ebbb52203b87fad0817
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-06 15:10:28 +0100

    placeholder added

--- client/src/views/UserProfile.vue
+++ client/src/views/UserProfile.vue
@@ -83,7 +83,7 @@
             role="tabpanel"
             aria-labelledby="nav-my-flights-tab"
           >
-            ...
+            Such empty 😞
           </div>
         </div>
       </div>
