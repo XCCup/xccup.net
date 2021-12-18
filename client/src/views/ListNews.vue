1961a501ee1205e99df9c359244d9771e9284ec9
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-18 18:37:36 +0100

    News added

---
+++ client/src/views/ListNews.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="newsItems" data-cy="list-news" class="container py-4">
+    <h2 class="pb-2 border-bottom">News</h2>
+    <div class="row mt-3">
+      <div
+        v-for="newsItem in newsItems"
+        :key="newsItem.id"
+        data-cy="news-item"
+        class="mt-3"
+      >
+        <NewsItem :news-item="newsItem" :snip-text="false" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ApiService from "@/services/ApiService.js";
+import { ref } from "vue";
+import { setWindowName } from "../helper/utils";
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+setWindowName("News");
+
+const newsItems = ref(null);
+try {
+  const res = await ApiService.getPublicNews();
+  newsItems.value = res.data;
+} catch (error) {
+  console.log(error);
+  router.push({
+    name: "NetworkError",
+  });
+}
+</script>
+
+<style scoped></style>
