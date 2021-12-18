328dca774faa6ddf93872bede4dd78abe4803c08
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-18 18:36:07 +0100

    vue snip removed

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import VueSnip from "vue-snip";
 
 let app = createApp(App);
 app.use(router);
-app.use(VueSnip);
-
 app.mount("#app");
