0332576f0a62dd2b3adc1906ff8c73022ab462cd
Kai Wissel <kai.wpkt@gmail.com>
2021-12-20 12:49:47 +0100

    Backend changes for teams

--- server/model/Team.js
+++ server/model/Team.js
@@ -9,19 +9,27 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: false, // It's allowed that the same name can appear in two different years
     },
     participantInSeasons: {
       type: DataTypes.ARRAY(DataTypes.INTEGER),
     },
+    // Create a complete new team entry if the compostion of members changes
+    members: {
+      type: DataTypes.ARRAY(DataTypes.UUID),
+      defaultValue: [],
+    },
   });
-  Team.associate = (models) => {
-    Team.hasMany(models.User, {
-      as: "members",
-      foreignKey: {
-        name: "teamId",
-      },
-    });
-  };
+
+  // ??? Don't make this association because a user can change his association in a later season ???
+  // Team.associate = (models) => {
+  //   Team.hasMany(models.User, {
+  //     as: "members",
+  //     foreignKey: {
+  //       name: "teamId",
+  //     },
+  //   });
+  // };
 
   return Team;
 };
