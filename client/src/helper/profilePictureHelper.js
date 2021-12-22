42c4a9fbfb3ae601a8d2a7f382aa0bd8509d3f9a
KaiWissel <kai.wpkt@gmail.com>
2021-12-22 20:47:22 +0100

    Prevent calling when no userId set

--- client/src/helper/profilePictureHelper.js
+++ client/src/helper/profilePictureHelper.js
@@ -1,6 +1,7 @@
 import { getbaseURL } from "./baseUrlHelper";
 
 export function createUserPictureUrl(userId, thumb) {
+  if (!userId) return;
   const thumbExtension = thumb ? "?thumb=true" : "";
   return `${getbaseURL()}users/picture/${userId}${thumbExtension}`;
 }
