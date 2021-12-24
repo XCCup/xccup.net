4e949e26419b8b6607691f207f1a33c8d1b8f54e
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-24 20:57:30 +0100

    todo added

--- client/src/components/UserProfileMyFlights.vue
+++ client/src/components/UserProfileMyFlights.vue
@@ -1,5 +1,6 @@
 <template>
   <!-- TODO: Add filter spinner when loading -->
+  <!-- TODO: How many flights shall be shown -->
   <div class="row mt-3">
     <div class="col-6"></div>
     <div class="col-6">
