8564031055ff60eb92e032a71308c6c606ac726e
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-29 13:31:14 +0100

    class removed

--- client/src/components/NetworkError.vue
+++ client/src/components/NetworkError.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="container text-center my-5">
-    <div class="networkError">
-      <h1>Uh-Oh!</h1>
+    <h1>Uh-Oh!</h1>
 
-      <h3>
-        Da ist was schief gelaufen :( <br />
-        <a href="#" @click="$router.go(-1)">Hier klicken</a> um es noch mal zu
-        probieren
-      </h3>
-    </div>
+    <h3>
+      Da ist was schief gelaufen :( <br />
+      <a href="#" @click="$router.go(-1)">Hier klicken</a> um es noch mal zu
+      probieren
+    </h3>
   </div>
 </template>
