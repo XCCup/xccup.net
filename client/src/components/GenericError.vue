615a698f23336e487ee051fdef66ca14cb4fa693
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-29 13:29:50 +0100

    generic error added

---
+++ client/src/components/GenericError.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Uh-Oh!</h1>
+
+  <h3>
+    Da ist was schief gelaufen :( <br />
+    <a href="#" @click="$router.go(0)">Hier klicken</a> um es noch mal zu
+    probieren
+  </h3>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
