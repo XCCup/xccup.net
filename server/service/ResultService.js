c59e68b1aa718c9744617c5b94a72f47661d1f95
Kai Wissel <kai.wpkt@gmail.com>
2022-02-03 20:29:04 +0100

    Added late bird endpoint

--- server/service/ResultService.js
+++ server/service/ResultService.js
@@ -23,6 +23,7 @@ const {
   REMARKS_SENIOR,
   REMARKS_TEAM,
   REMARKS_EARLYBIRD,
+  REMARKS_LATEBIRD,
 } = require("../constants/result-remarks-constants");
 const moment = require("moment");
 
@@ -194,6 +195,27 @@ const service = {
       resultSingleUserEntries,
       {
         REMARKS: REMARKS_EARLYBIRD,
+      },
+      20
+    );
+  },
+
+  getLateBird: async (year, region) => {
+    const seasonDetail = await retrieveSeasonDetails(year);
+
+    const endDate = seasonDetail.endDate;
+    const startDate = moment(endDate).subtract(3, "months");
+    const where = createDefaultWhereForFlight({ startDate, endDate });
+    const sortOrder = [["takeoffTime", "DESC"]];
+
+    const resultQuery = await queryDb({ where, region, sortOrder });
+    const result = resultQuery.map((r) => r.toJSON());
+    const resultSingleUserEntries = removeMultipleEntriesForUsers(result);
+
+    return addConstantInformationToResult(
+      resultSingleUserEntries,
+      {
+        REMARKS: REMARKS_LATEBIRD,
       },
       20
     );
