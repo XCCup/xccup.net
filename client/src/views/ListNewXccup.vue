327e7d65a4b76e8970fe135ffbcdc1f8b5664c4e
Stephan Schöpe <socialcrap+github@posteo.de>
2022-02-08 09:41:48 -0500

    description of features updated

---
+++ client/src/views/ListNewXccup.vue
@@ -0,0 +1,149 @@
+<template>
+  <div data-cy="list-new-xccup" class="container py-4">
+    <h3>Der neue XCCup</h3>
+    Im letzten Jahr hatte Wolf bereits angekündigt, dass unser XCCup Entwickler
+    Sascha den Posten nach vielen Jahren gerne abgeben würde und ein Nachfolger
+    gesucht wird.<br />
+
+    Diesem Aufruf sind wir (Kai & Stephan) als langjährige XCCup Teilnehmer
+    gefolgt.
+    <p />
+    Da die aktuelle Webseite aus einer Zeit stammt, in der Smartphones noch kein
+    Thema waren ist die Bedienung auf mobilen Geräten eher umständlich gewesen.
+    Aus diesem Grund haben wir den XCCup mit Fokus auf mobile Geräte von neu auf
+    programmiert.
+    <br />
+    So gut wie alle Funktionen haben wir dabei übernommen und zusätzlich noch
+    einige Neue hinzugefügt.
+    <p />
+
+    Vor dem ersten Login müsst ihr auf "Passwort vergessen" klicken und ein
+    neues Passwort anfordern. Verwendet dazu bitte eure im alten XCCup benutzte
+    E-Mail-Adresse.
+
+    <p />
+
+    <h5>Neue Funktionen:</h5>
+    <p />
+
+    - Es ist nun möglich auf Kommentare zu antworten, statt einfach nur einen
+    Weiteren unter die Vorhandenen zu schreiben.
+    <p />
+    - Airbuddies: Solltet ihr zusammen mit anderen Piloten geflogen sein könnt
+    ihr dessen Trackklog einblenden. Ihr müsst nichts weiter tun, als euren Flug
+    hochzuladen und findet dann einen Button “Airbuddies” unter dem Barogramm.
+    (Natürlich nur, wenn der andere Pilot auch seinen Fluch hochgeladen hat…)
+    <p />
+    - Die Tabellen der Wertungen zeigen abhängig von der Gerätegröße
+    unterschiedlich viele Details an.
+    <p />
+    - Ihr könnt einen Hike&Fly Flug als solchen kennzeichnen. Die Höhenmeter
+    werden in den Flugdetails angezeigt.
+    <p />
+    - Ihr könnt jetzt unterschiedliche Schirme im Profil hinterlegen und aus
+    diesen beim Hochladen eines neuen Fluges auswählen.
+    <p />
+    - "Dark mode" abhängig von euren Geräteeinstellungen. (Dieser ist was die
+    Farben angeht noch nicht ganz perfekt…)
+    <p />
+    - Eine Übersicht der Flüge des aktuellen Tages inkl. Karte.
+    <p />
+    - Die Validität der IGC-Datei wird nach dem Hochladen geprüft (G-Record)
+    <p />
+    - Eine offene API um ggf. Daten für die eigene Vereinswertung leichter
+    auszulesen
+    <p />
+
+    Das komplette Projekt ist "Open Source" - wer möchte, kann sich gerne
+    einbringen:
+    <a href="https://github.com/XCCup/xccup.net" target="_blank"
+      >https://github.com/XCCup/xccup.net</a
+    >
+    <p />
+
+    Es gab aber auch Änderungen, auf die wir explizit hinweisen möchten. Hier
+    müsst ihr ggf. selbst tätig werden.
+    <p />
+    1. Geburtstag: <br />Der Geburtstag war in der alten Plattform nicht bei
+    jedem Teilnehmer korrekt im Profil hinterlegt. Falls ihr an der
+    Seniorenwertung teilnehmen wollt, müsst ihr diesen auf Korrektheit in Eurem
+    Profil prüfen.
+    <p />
+    2. RLP Pokal / Lux Championat: <br />Bisher musstet ihr für die Teilnahme an
+    diesen Wertungen einen entsprechenden Haken in Eurem Profil setzen. Diesen
+    Haken gibt es nun nicht mehr. Ihr nehmt automatisch an diesen Wertungen
+    teil, wenn ihr das entsprechende (Bundes-)Land in Eurem Profil eingestellt
+    habt.
+    <p />
+    3. Qualifikation Mosel Open RLP/SL/NRW: <br />Für die Mosel Open werden
+    jedes Jahr Plätze für Teilnehmer aus dem Saarland, Rheinland-Pfalz und
+    Nordrhein-Westfalen reserviert. Die Vergabe der Plätze für das Saarland,
+    Rheinland-Pfalz erfolgte bislang schon über den XCCup. Dies soll in Zukunft
+    auch für NRW erfolgen. Dazu müsst ihr ebenfalls das Bundesland in Eurem
+    Profil korrekt eintragen.
+    <p />
+    4. T-Shirt Versand: <br />Ihr habt jetzt ebenfalls die Möglichkeit, Eure
+    vollständige postalische Anschrift in Eurem Profil zu hinterlegen. Damit
+    haben wir als XCCup in Zukunft die Möglichkeit, Euch Eure erflogenen XCCup
+    T-Shirts zum Jahresende per Post zu zusenden.
+    <p />
+    5. Startplätze: <br />In der Vergangenheit war es möglich, bei einer
+    Streckenmeldung manuell einen Startplatz einzugeben. Diese Möglichkeit
+    besteht aus diversen Gründen nicht mehr. Es gibt jedoch die Möglichkeit über
+    Formulare einen neuen Startplatz an uns zu melden. Welche Startplätze
+    aktuell im System hinterlegt sind, seht ihr in der Fluggebietsübersicht
+    unter Listen.
+    <p />
+    6. Streckenmeldungen: <br />Eine Streckenmeldung ist nur noch mit einem
+    gültigen IGC-File möglich. Eine Meldung mittels Start-/Landezeugen ist nicht
+    mehr möglich.
+
+    <p />
+
+    Die letzten Konfigurationen werden wir im Zeitraum 08.-11.02.2022
+    durchführen. In dieser Zeit wird die Plattform nicht immer erreichbar sein.
+    Danach werdet ihr die „neue“ Plattform unter der Euch bekannten URL
+    <a href="https://www.xccup.net" target="_blank">https://www.xccup.net</a>
+    erreichen. Die „alte“ Plattform wird noch eine Zeit lang als Archiv – bis
+    alle Daten übertragen worden sind – betrieben. Die „alte“ Plattform könnt
+    Ihr dann unter
+    <a href="https://archiv.xccup.net" target="_blank"
+      >https://archiv.xccup.net</a
+    >
+    erreichen. Streckenmeldungen in der „alten“ Welt werden nicht für die
+    kommende Saison gezählt. Es ist hier die neue Plattform zu nutzen.
+    <p />
+    Flüge, welche Ihr im Jahr 2022 eingereicht habt, werden wir nicht
+    übernehmen. Es wird aber bis Ende Februar möglich sein, Flüge, welche älter
+    als 14 Tage und aus dem Jahr 2022 stammen erneut, einzureichen.
+    <p />
+    Falls Ihr Euch auf der Testplattform noch nicht eingeloggt hattet, müsst ihr
+    beim ersten Login auf "Passwort vergessen" klicken und ein neues Passwort
+    anfordern. Verwendet dazu bitte Eure im alten XCCup benutzte E-Mail-Adresse.
+    <br />
+    <p />
+  </div>
+</template>
+
+<script setup>
+import ApiService from "@/services/ApiService.js";
+import { ref } from "vue";
+import { setWindowName } from "../helper/utils";
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+setWindowName("News");
+
+const newsItems = ref(null);
+try {
+  const res = await ApiService.getPublicNews();
+  newsItems.value = res.data;
+} catch (error) {
+  console.log(error);
+  router.push({
+    name: "NetworkError",
+  });
+}
+</script>
+
+<style scoped></style>
