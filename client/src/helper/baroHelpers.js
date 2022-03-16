26bef84886085d89cc42648ba4bdde6cd084b71d
Stephan Schöpe <socialcrap+github@posteo.de>
2022-03-16 15:16:05 +0100

    buddies are back!

--- client/src/helper/baroHelpers.js
+++ client/src/helper/baroHelpers.js
@@ -40,34 +40,34 @@ export function processBaroData(flight, buddyTracks) {
   };
   // Dataset for main flight
   allBaroData[1] = {
-    label: "Pilot 1",
+    label: "Pilot",
     data: baroData,
     backgroundColor: TRACK_COLORS[0],
     borderColor: TRACK_COLORS[0],
   };
   // Datasets for all aribuddies
-  // if (buddyTracks) {
-  //   buddyTracks.forEach((element, index) => {
-  //     const buddyBaro = [];
-  //     // Check if this track is activated
-  //     if (element.isActive) {
-  //       for (var i = 0; i < element.fixes.length; i++) {
-  //         buddyBaro.push({
-  //           x: element.fixes[i].timestamp,
-  //           y: element.fixes[i].gpsAltitude,
-  //           speed: element.fixes[i].speed,
-  //           climb: element.fixes[i].climb,
-  //         });
-  //       }
-  //     }
-  //     // Create the buddy dataset
-  //     allBaroData[index + 2] = {
-  //       label: element.buddyName,
-  //       data: buddyBaro,
-  //       backgroundColor: TRACK_COLORS[index + 1],
-  //       borderColor: TRACK_COLORS[index + 1],
-  //     };
-  //   });
-  // }
+  if (buddyTracks) {
+    buddyTracks.forEach((element, index) => {
+      const buddyBaro = [];
+      // Check if this track is activated
+      if (element.isActive) {
+        for (var i = 0; i < element.fixes.length; i++) {
+          buddyBaro.push({
+            x: element.fixes[i].timestamp,
+            y: element.fixes[i].gpsAltitude,
+            speed: element.fixes[i].speed,
+            climb: element.fixes[i].climb,
+          });
+        }
+      }
+      // Create the buddy dataset
+      allBaroData[index + 2] = {
+        label: element.buddyName,
+        data: buddyBaro,
+        backgroundColor: TRACK_COLORS[index + 1],
+        borderColor: TRACK_COLORS[index + 1],
+      };
+    });
+  }
   return allBaroData;
 }
