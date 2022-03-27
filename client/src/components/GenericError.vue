8931ccf3319cbfd15d619e2af815b4bed4b274e9
KaiWissel <kai.wpkt@gmail.com>
2022-03-27 16:48:32 +0200

    improved styling

--- client/src/components/GenericError.vue
+++ client/src/components/GenericError.vue
@@ -7,24 +7,22 @@
       <a href="#" @click.prevent="$router.go(0)">Hier klicken</a> um es noch mal
       zu probieren
     </h3>
-    <p>
-      <button
-        class="btn btn-primary btn-sm me-2 mb-2"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#debugCollapse"
-        aria-expanded="false"
-        aria-controls="debugCollapse"
-      >
-        Debug Informationen anzeigen
-      </button>
+    <button
+      class="btn btn-primary btn-sm col-5 me-3 mb-3"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#debugCollapse"
+      aria-expanded="false"
+      aria-controls="debugCollapse"
+    >
+      Debug Informationen anzeigen
+    </button>
 
-      <a
-        class="btn btn-primary btn-sm"
-        :href="`mailto:${ADMIN_EMAIL}?body=${plainTextError}&subject=ErrorDebug`"
-        >Debug Informationen per e-mail versenden</a
-      >
-    </p>
+    <a
+      class="btn btn-primary btn-sm col-5 mb-3"
+      :href="`mailto:${ADMIN_EMAIL}?body=${plainTextError}&subject=ErrorDebug`"
+      >Debug Informationen per E-Mail versenden</a
+    >
     <div id="debugCollapse" class="collapse">
       <div class="card card-body">
         {{ plainTextError }}
