7745cd6fb0e00c92a2a33ee2f31e77f7ca5a23c7
KaiWissel <kai.wpkt@gmail.com>
2022-03-31 13:32:37 +0200

    fixed default case

--- client/src/helper/profilePictureHelper.js
+++ client/src/helper/profilePictureHelper.js
@@ -1,6 +1,6 @@
 import { getbaseURL } from "./baseUrlHelper";
 
-export function createUserPictureUrl(userId, options) {
+export function createUserPictureUrl(userId, options = {}) {
   if (!userId) return;
   const size = options.size ? `?size=${options.size}` : "";
   return `${getbaseURL()}users/picture/${userId}${size}`;
