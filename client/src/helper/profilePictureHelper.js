73357152ce32b44cde5b3e4e7be00eb4a10ec083
Stephan Schöpe <socialcrap+github@posteo.de>
2022-03-31 13:33:54 +0200

    null catched

--- client/src/helper/profilePictureHelper.js
+++ client/src/helper/profilePictureHelper.js
@@ -2,6 +2,6 @@ import { getbaseURL } from "./baseUrlHelper";
 
 export function createUserPictureUrl(userId, options) {
   if (!userId) return;
-  const size = options.size ? `?size=${options.size}` : "";
+  const size = options?.size ? `?size=${options?.size}` : "";
   return `${getbaseURL()}users/picture/${userId}${size}`;
 }
