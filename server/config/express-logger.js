8349fb75772fef8e3bebcd1865f4bf094943f38c
Stephan Schöpe <socialcrap+github@posteo.de>
2022-04-03 23:01:40 +0200

    import corrected

--- server/config/express-logger.js
+++ server/config/express-logger.js
@@ -1,6 +1,6 @@
 const morgan = require("morgan");
-const config = require("./env-config");
-const logger = require("./logger");
+const config = require("./env-config").default;
+const logger = require("./logger").default;
 
 // dev format also colorizes status code which leads to ASCII color codes when logging to a file
 const morganLogFormat = config.get("env") === "development" ? "dev" : "tiny";
