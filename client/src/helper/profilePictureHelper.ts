eb21a14a51f4e96e9dd25f6007b62b050d4ade9d
Stephan Schöpe <socialcrap+github@posteo.de>
2022-04-10 12:34:34 +0200

    profilePictureHelper => TS

---
+++ client/src/helper/profilePictureHelper.ts
@@ -0,0 +1,10 @@
+import { getbaseURL } from "./baseUrlHelper";
+
+export function createUserPictureUrl(
+  userId: string,
+  options?: { size: string }
+) {
+  if (!userId) return;
+  const size = options?.size ? `?size=${options?.size}` : "";
+  return `${getbaseURL()}users/picture/${userId}${size}`;
+}
