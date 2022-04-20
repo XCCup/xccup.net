4ed82277a826613db52ff9bebd6eec4b98a8fdb3
Stephan Schöpe <socialcrap+github@posteo.de>
2022-04-20 17:35:03 +0200

    Generic error cleaned up from unuseful functions

--- client/src/components/GenericError.vue
+++ client/src/components/GenericError.vue
@@ -7,36 +7,11 @@
       <a href="#" @click.prevent="$router.go(0)">Hier klicken</a> um es noch mal
       zu probieren
     </h3>
-    <button
-      class="btn btn-primary btn-sm col-5 me-3 mb-3"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#debugCollapse"
-      aria-expanded="false"
-      aria-controls="debugCollapse"
-    >
-      Debug Informationen anzeigen
-    </button>
-
-    <a
-      class="btn btn-primary btn-sm col-5 mb-3"
-      :href="`mailto:${ADMIN_EMAIL}?body=${plainTextError}&subject=ErrorDebug`"
-      >Debug Informationen per E-Mail versenden</a
-    >
-    <div id="debugCollapse" class="collapse">
-      <div class="card card-body">
-        {{ plainTextError }}
-      </div>
-    </div>
   </div>
 </template>
 
-<script setup>
-import { GENERIC_ERROR, ADMIN_EMAIL } from "@/common/Constants";
-import { computed } from "vue";
-const props = defineProps({ error: { type: Object, default: null } });
-
-const plainTextError = computed(() => JSON.stringify(props.error));
+<script setup lang="ts">
+import { GENERIC_ERROR } from "@/common/Constants";
 </script>
 
 <style scoped></style>
