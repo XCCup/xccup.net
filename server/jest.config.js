01fd1738e80c7d6af018745252e0ce0bb1ea100a
Stephan Schöpe <socialcrap+github@posteo.de>
2022-05-07 22:20:27 +0200

    test fixed

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["<rootDir>/test/setup-tests.js"],
+  // Only needed b/c igc-parser fork contains non build ts files
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!igc-parser/)"],
 };
