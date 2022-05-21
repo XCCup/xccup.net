c228a9a70bb115e7030a7f04f023953a84a9f77c
KaiWissel <kai.wpkt@gmail.com>
2022-05-21 12:15:09 +0200

    removed unused code

--- server/helper/Utils.js
+++ server/helper/Utils.js
@@ -27,14 +27,6 @@ function generateRandomString() {
   );
 }
 
-async function waitTillDbHasSync() {
-  const RETRY_TIMEOUT = 3000;
-  while (process.env.DB_SYNC_IN_PROGRESS == "true") {
-    logger.info("Will wait till DB syncing has finished");
-    await sleep(RETRY_TIMEOUT);
-  }
-  logger.info("DB syncing finished");
-}
 function sanitize(text) {
   return sanitizeHtml(text, {
     // Do not allow html at all
@@ -47,6 +39,5 @@ exports.sanitizeHtml = sanitize;
 exports.sleep = sleep;
 exports.getCurrentYear = getCurrentYear;
 exports.arrayRemove = arrayRemove;
-exports.waitTillDbHasSync = waitTillDbHasSync;
 exports.findKeyByValue = findKeyByValue;
 exports.generateRandomString = generateRandomString;
