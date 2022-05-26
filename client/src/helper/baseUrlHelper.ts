a7dce01bdae2e4647e62621afaf9c67b74188958
Stephan Schöpe <socialcrap+github@posteo.de>
2022-05-26 09:34:58 +0200

    bseUrlHelper refactored

--- client/src/helper/baseUrlHelper.ts
+++ client/src/helper/baseUrlHelper.ts
@@ -1,8 +1,14 @@
-export function getbaseURL() {
+export function getbaseURL(): string {
+  if (
+    import.meta.env.MODE != "production" &&
+    import.meta.env.VITE_BASE_USE_LIVE_API != "true"
+  )
+    return `${location.protocol}//${location.hostname}:3000/api/`;
+
+  if (import.meta.env.VITE_BASE_API) return import.meta.env.VITE_BASE_API;
+
   if (import.meta.env.MODE == "production")
     return `${location.protocol}//${location.hostname}/api/`;
 
-  if (import.meta.env.VITE_BASE_USE_LIVE_API == "true")
-    return "https://xccup.net/api/";
   return `${location.protocol}//${location.hostname}:3000/api/`;
 }
