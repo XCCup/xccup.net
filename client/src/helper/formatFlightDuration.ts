5655abe29c53e2da53e357b8e7328d25e0e1953c
Kai Wissel <kai.wpkt@gmail.com>
2022-07-17 19:59:31 +0200

    removed comments

--- client/src/helper/formatFlightDuration.ts
+++ client/src/helper/formatFlightDuration.ts
@@ -1,7 +1,6 @@
 export function formatFlightDuration(duration: number | undefined): string {
   if (!duration) return "";
   const ms = duration * 60 * 1000;
-  // let seconds = parseInt((ms / 1000) % 60);
 
   //@ts-expect-error
   let minutes: number | string = parseInt((ms / (1000 * 60)) % 60);
@@ -10,6 +9,6 @@ export function formatFlightDuration(duration: number | undefined): string {
   let hours = parseInt((ms / (1000 * 60 * 60)) % 24);
   minutes = minutes < 10 ? "0" + minutes : minutes;
 
-  // seconds = seconds < 10 ? "0" + seconds : seconds;
   return hours + ":" + minutes + "h";
 }
+
