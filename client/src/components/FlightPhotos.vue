5ec3cf11139447de5e52e47af8cd518536c07fb8
Stephan Schöpe <socialcrap+github@posteo.de>
2022-08-20 16:01:27 +0200

    only show photo upload wrning on edit page

--- client/src/components/FlightPhotos.vue
+++ client/src/components/FlightPhotos.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mt-3">
-    <div v-if="isAdmin" class="text-warning">
+    <div v-if="isAdmin && flightId" class="text-warning mb-4">
       <!-- TODO: Find a nice way to prevent mixing up user ids when uploading for a user or simply do not upload for a user… -->
       Admins should not upload photos for a user!
     </div>
