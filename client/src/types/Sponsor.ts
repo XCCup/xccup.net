1996bc2bef8219ac48a667cc4ffeceb6a16c7a45
Stephan Schöpe <socialcrap+github@posteo.de>
2022-10-09 21:56:30 +0200

    admin sponsor hotfix

--- client/src/types/Sponsor.ts
+++ client/src/types/Sponsor.ts
@@ -1,21 +1,21 @@
 export interface Sponsor {
-  id?: string;
-  name: string;
-  website: string;
-  tagline: string;
-  isGoldSponsor: boolean;
-  sponsorInSeasons: number[];
-  contact: Contact;
+  id?: string; // Why is this optional?
+  name: string; // Is this safe?
+  website?: string;
+  tagline?: string;
+  isGoldSponsor?: boolean;
+  sponsorInSeasons: number[]; // is This safe?
+  contact?: Contact;
   updatedAt?: Date;
   createdAt?: Date;
   logo?: Logo;
 }
 
 export interface Contact {
-  address: string;
-  email: string;
-  phone: string;
-  phone2: string;
+  address?: string;
+  email?: string;
+  phone?: string;
+  phone2?: string;
 }
 
 export interface Logo {
