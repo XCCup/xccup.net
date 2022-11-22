0f100f66703fb02dc44ceb6cbfccf5b8c3029598
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-22 13:41:57 +0100

    test fixed

--- server/test/ElevationAttacher.test.ts
+++ server/test/ElevationAttacher.test.ts
@@ -34,14 +34,14 @@ test("Attach elevation data to position fixes and check values", async () => {
 
   expect(fixes[0].elevation).toBeGreaterThanOrEqual(firstFix.elevation);
 
-  expect(fixes[0].elevation).toBeLessThan(firstFix.elevation);
+  expect(fixes[0].elevation).toBe(firstFix.elevation);
   expect(fixes[0].latitude).toBe(firstFix.latitude);
   expect(fixes[0].longitude).toBe(firstFix.longitude);
 
   expect(fixes[fixes.length - 1].elevation).toBeGreaterThanOrEqual(
     lastFix.elevation
   );
-  expect(fixes[fixes.length - 1].elevation).toBeLessThan(lastFix.elevation);
+  expect(fixes[fixes.length - 1].elevation).toBe(lastFix.elevation);
   expect(fixes[fixes.length - 1].latitude).toBe(lastFix.latitude);
   expect(fixes[fixes.length - 1].longitude).toBe(lastFix.longitude);
 
