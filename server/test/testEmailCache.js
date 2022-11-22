c9023fcb4d6807b751f35fc2cd87013f96788167
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-22 11:39:43 +0100

    clearer logging

--- server/test/testEmailCache.js
+++ server/test/testEmailCache.js
@@ -5,10 +5,12 @@ const cache = [];
 
 exports.push = (email) => {
   cache.push(email);
+  logger.error("Add mail");
   logger.error(email);
 };
 
 exports.findLatestForToUser = (toUserEmail) => {
+  logger.error("Get last mail");
   logger.error(findLast(cache, (e) => e.to?.includes(toUserEmail)));
 
   return findLast(cache, (e) => e.to?.includes(toUserEmail));
