0e65cb2ae0cadaec279170bbcdefa3578883df5a
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-22 12:31:04 +0100

    even more logging

--- server/test/testEmailCache.js
+++ server/test/testEmailCache.js
@@ -4,13 +4,14 @@ const logger = require("../config/logger");
 const cache = [];
 
 exports.push = (email) => {
+  if (!email) return logger.error("No content");
   cache.push(email);
-  logger.error("Add mail");
-  logger.error(email);
+  logger.error("Add mail to cache: " + email.to);
 };
 
 exports.findLatestForToUser = (toUserEmail) => {
-  logger.error("Get last mail: ", toUserEmail);
+  if (!toUserEmail) return logger.error("No email specified");
+  logger.error("Get last mail from cache: " + toUserEmail);
   logger.error(findLast(cache, (e) => e.to?.includes(toUserEmail)));
 
   return findLast(cache, (e) => e.to?.includes(toUserEmail));
