c584ff80a5076b7ff7c9b28a0d40e94db6d9b6f7
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-22 11:22:04 +0100

    log server

--- server/test/testEmailCache.js
+++ server/test/testEmailCache.js
@@ -1,11 +1,15 @@
 const { findLast } = require("lodash");
+const logger = require("../config/logger");
 
 const cache = [];
 
 exports.push = (email) => {
   cache.push(email);
+  logger.error(email);
 };
 
 exports.findLatestForToUser = (toUserEmail) => {
+  logger.error(findLast(cache, (e) => e.to?.includes(toUserEmail)));
+
   return findLast(cache, (e) => e.to?.includes(toUserEmail));
 };
