0af4f84f0ea4c686decca1b7e6f0fc0984572699
Stephan Schöpe <socialcrap+github@posteo.de>
2022-11-26 16:35:33 +0100

    initial commit

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://af74b459444f4ce6a9da5ed70b971f6e@sentry.xccup.net/1",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ["localhost", "xccup.net", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 app.use(router);
 app.mount("#app");
