1792c3e63ebd08684fd86dd106317de4768bc5bf
Stephan Schöpe <socialcrap+github@posteo.de>
2022-12-04 12:11:00 +0100

    sentry in dev removed

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,26 +8,28 @@ import { BrowserTracing } from "@sentry/tracing";
 
 const app = createApp(App);
 
-Sentry.init({
-  app,
-  dsn: import.meta.env.VITE_SENTRY_URL,
-  integrations: [
-    new BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracePropagationTargets: [
-        "localhost",
-        "xccup.net",
-        "render.xccup.net",
-        /^\//,
-      ],
-    }),
-  ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-  logErrors: true,
-});
+if (import.meta.env.MODE == "production") {
+  Sentry.init({
+    app,
+    dsn: import.meta.env.VITE_SENTRY_URL,
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracePropagationTargets: [
+          "localhost",
+          "xccup.net",
+          "render.xccup.net",
+          /^\//,
+        ],
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    logErrors: true,
+  });
+}
 
 app.use(router);
 app.mount("#app");
