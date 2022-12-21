02ec955e10763ae6f60f85e3976963d604cce5f1
Stephan Schöpe <socialcrap+github@posteo.de>
2022-12-21 09:51:24 +0100

    test fixed

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -66,6 +66,7 @@ Cypress.Commands.add("typeTextareaInModal", (modalSelector, text) => {
  * Logs in a user with a role of "Administrator" (Camille Schaden).
  */
 Cypress.Commands.add("loginAdminUser", () => {
+  cy.visit("/");
   cy.get("#loginNavButton").click();
   cy.url().should("include", "/login");
   cy.get("h3").should("have.text", "Login");
@@ -82,6 +83,7 @@ Cypress.Commands.add("loginAdminUser", () => {
  * Logs in a user with no role (Ramona Gislason).
  */
 Cypress.Commands.add("loginNormalUser", () => {
+  cy.visit("/");
   cy.get("#loginNavButton").click();
   cy.url().should("include", "/login");
   cy.get("h3").should("have.text", "Login");
@@ -98,6 +100,7 @@ Cypress.Commands.add("loginNormalUser", () => {
  * Logs a user with the provided email and password in to the website.
  */
 Cypress.Commands.add("login", (email, password) => {
+  cy.visit("/");
   cy.get("#loginNavButton").click();
   cy.url().should("include", "/login");
   cy.get("h3").should("have.text", "Login");
@@ -114,6 +117,7 @@ Cypress.Commands.add("login", (email, password) => {
  * Logs the current user user out of the website.
  */
 Cypress.Commands.add("logout", () => {
+  cy.visit("/");
   cy.get("#userNavDropdownMenu").click();
   cy.get("li").contains("Abmelden").click();
   cy.get("#loginNavButton").should("includes.text", "Login");
