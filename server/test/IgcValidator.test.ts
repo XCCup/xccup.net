0f26ae3e6b88d3b7bbd8e62d990abd4f2debed02
KaiWissel <kai.wpkt@gmail.com>
2023-06-28 15:00:17 +0200

    unit tests without specific encoding

--- server/test/IgcValidator.test.ts
+++ server/test/IgcValidator.test.ts
@@ -2,18 +2,17 @@
  * @jest-environment node
  */
 import path from "path";
+import fs from "fs";
 import { validateIgc } from "../igc/IgcValidator";
-import { readIgcFile } from "../igc/IgcFileRead";
+// import { readIgcFile } from "../igc/IgcFileRead";
 
 test("Validate an igc-File which should result to PASSED", (done) => {
-  const { content, encoding } = readFile("kai_fai", "fai_60km42_3h53m.igc");
-
   const igc = {
     name: "AValidFile.igc",
-    body: content,
+    path: createFilePath("kai_fai", "fai_60km42_3h53m.igc"),
   };
 
-  validateIgc(igc.body, igc.name)
+  validateIgc(igc.path, igc.name)
     .then((result) => {
       expect(result).toBe("PASSED");
       done();
@@ -22,14 +21,12 @@ test("Validate an igc-File which should result to PASSED", (done) => {
 });
 
 test("Validate an igc-File which should result to PASSED (Win1252 Encoding)", (done) => {
-  const { content, encoding } = readFile("non-utf8", "Win1252.igc");
-
   const igc = {
     name: "AValidFile.igc",
-    body: content,
+    path: createFilePath("non-utf8", "Win1252.igc"),
   };
 
-  validateIgc(igc.body, igc.name, encoding)
+  validateIgc(igc.path, igc.name)
     .then((result) => {
       expect(result).toBe("PASSED");
       done();
@@ -38,17 +35,12 @@ test("Validate an igc-File which should result to PASSED (Win1252 Encoding)", (d
 });
 
 test("Validate an igc-File which should result to FAILED", (done) => {
-  const { content, encoding } = readFile(
-    "kai_fai_invalid",
-    "removed_line_20to22.igc"
-  );
-
   const igc = {
     name: "AInvalidFile.igc",
-    body: content,
+    path: createFilePath("kai_fai_invalid", "removed_line_20to22.igc"),
   };
 
-  validateIgc(igc.body, igc.name, encoding)
+  validateIgc(igc.path, igc.name)
     .then((result) => {
       expect(result).toBe("FAILED");
       done();
@@ -60,5 +52,11 @@ function readFile(folder: string, file: string) {
   const parentDir = path.resolve(__dirname, "..");
   const igcFilePath = path.join(parentDir, "/igc/demo_igcs", folder, file);
 
-  return readIgcFile(igcFilePath);
+  // return readIgcFile(igcFilePath);
+  return fs.readFileSync(igcFilePath);
+}
+
+function createFilePath(folder: string, file: string) {
+  const parentDir = path.resolve(__dirname, "..");
+  return path.join(parentDir, "/igc/demo_igcs", folder, file);
 }
