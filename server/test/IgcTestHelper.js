f797de86df1400226cabe87281239e0ad8cf399a
KaiWissel <kai.wpkt@gmail.com>
2023-07-02 19:56:38 +0200

    readded encoding option

--- server/test/IgcTestHelper.js
+++ server/test/IgcTestHelper.js
@@ -3,7 +3,10 @@ const path = require("path");
 
 const readIgcFile = (folder, file) => {
   const parentDir = path.resolve(__dirname, "..");
-  return fs.readFileSync(path.join(parentDir, "/igc/demo_igcs", folder, file));
+  return fs.readFileSync(
+    path.join(parentDir, "/igc/demo_igcs", folder, file),
+    "utf8"
+  );
 };
 
 exports.readIgcFile = readIgcFile;
