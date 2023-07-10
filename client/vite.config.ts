3ca1f53071a0e7d4e2a1417646bea265708860f2
Stephan Schöpe <socialcrap+github@posteo.de>
2023-07-10 14:35:36 +0200

    vite config refined

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
@@ -8,49 +8,57 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: ["vue", "vue/macros", "vue-router", "@vueuse/core"],
-      dts: true,
-      dirs: ["./src/composables"],
-      vueTemplate: true,
-    }),
-
-    // https://github.com/antfu/vite-plugin-components
-    Components({
-      dts: true,
-    }),
-    sentryVitePlugin({
-      org: "xccup",
-      project: "xccup",
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@/": `${path.resolve(__dirname, "src")}/`,
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-    },
-  },
-  server: {
-    port: 8000,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Import custom bootstrap variable overrides in every component
-        additionalData: [`@import "@/styles/shared.scss";`],
-        charset: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    plugins: [
+      vue(),
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: ["vue", "vue/macros", "vue-router", "@vueuse/core"],
+        dts: true,
+        dirs: ["./src/composables"],
+        vueTemplate: true,
+      }),
+  
+      // https://github.com/antfu/vite-plugin-components
+      Components({
+        dts: true,
+      }),
+      sentryVitePlugin({
+        org: "xccup",
+        project: "xccup",
+        authToken: env.SENTRY_AUTH_TOKEN,
+        url: env.SENTRY_URL
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
       },
     },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [visualizer()],
+    server: {
+      port: 8000,
     },
-    sourcemap: true,
-  },
-});
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Import custom bootstrap variable overrides in every component
+          additionalData: [`@import "@/styles/shared.scss";`],
+          charset: false,
+        },
+      },
+    },
+    build: {
+      rollupOptions: {
+        plugins: [visualizer()],
+      },
+      sourcemap: true,
+    },
+  }
+})
+
