1ec03c7f0d8ec9e8845a0f3e170231ea8e8d5d2b
Stephan Schöpe <socialcrap+github@posteo.de>
2024-01-11 23:09:53 +0100

    cache user flarm ids

--- server/service/LivetrackingService.ts
+++ server/service/LivetrackingService.ts
@@ -8,6 +8,7 @@ import cron from "node-cron";
 
 const FLARM_URL = config.get("flarmUrl");
 const FLARM_API_KEY = config.get("flarmApiKey");
+const FLARM_USER_IDS_KEY = "user-flarm-ids";
 
 const cache = new NodeCache({ stdTTL: 600 });
 
@@ -71,6 +72,10 @@ const service = {
       };
     });
   },
+  flushFlarmIdCache: () => {
+    logger.info("LTS: Flushing user FLARM ID cache");
+    return cache.del(FLARM_USER_IDS_KEY);
+  },
 };
 
 async function fetchFlarmData() {
@@ -99,7 +104,7 @@ async function fetchFlarmData() {
 
     cache.set<ReducedFlightData>("flarm", reduced);
   } catch (error) {
-    console.log(error);
+    logger.error(error);
   }
 }
 
@@ -114,18 +119,30 @@ function reduceResolution(array: FlightData[], resolution: number) {
   });
 }
 
-async function getUserFlarmIds() {
+type FlarmIdWithName = {
+  flarmId: string | null | undefined;
+  name: string;
+};
+
+async function getUserFlarmIds(): Promise<FlarmIdWithName[]> {
+  const cached = cache.get<FlarmIdWithName[]>(FLARM_USER_IDS_KEY);
+
+  if (cached) return cached;
+
   const ids = await db.User.findAll({
     where: { flarmId: { [sequelize.Op.not]: null } },
     attributes: ["flarmId", "firstName", "lastName"],
   });
 
-  return ids.map((user) => {
+  const arrangedIds = ids.map((user) => {
     return {
       flarmId: user.flarmId,
       name: `${user.firstName} ${user.lastName}`,
     };
   });
+
+  cache.set(FLARM_USER_IDS_KEY, arrangedIds, 0); // Cache indefinitely
+  return arrangedIds;
 }
 
 module.exports = service;
