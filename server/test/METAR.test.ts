fa378e6391404fd4472b1029f946f411a4ba20cd
KaiWissel <kai.wpkt@gmail.com>
2024-02-10 12:13:24 +0100

    added in-between fixes

--- server/test/METAR.test.ts
+++ server/test/METAR.test.ts
@@ -7,6 +7,7 @@ test("Test that METAR service is responding correctly", (done) => {
   const spy = jest.spyOn(flightObject, "save");
   // Fixes are taken from the 104.igc file, which is also used in the frontend e2e test
   const flightFixes: FlightFixCombined[] = [
+    // Takeoff fix
     {
       timestamp: 1653214183000,
       time: "10:09:43",
@@ -15,6 +16,25 @@ test("Test that METAR service is responding correctly", (done) => {
       pressureAltitude: 484,
       gpsAltitude: 549,
     },
+    // Close after takeoff
+    {
+      timestamp: 1653214295000,
+      time: "10:11:35",
+      latitude: 51.2457,
+      longitude: 9.846316666666667,
+      pressureAltitude: 484,
+      gpsAltitude: 549,
+    },
+    // Close before landing
+    {
+      timestamp: 1653235236000,
+      time: "16:00:36",
+      latitude: 51.20783333333333,
+      longitude: 9.97175,
+      pressureAltitude: 502,
+      gpsAltitude: 622,
+    },
+    // Landing fix
     {
       timestamp: 1653235536000,
       time: "16:05:36",
@@ -35,9 +55,9 @@ test("Test that METAR service is responding correctly", (done) => {
       expect(spy).toHaveBeenCalled();
 
       // @ts-ignore
-      expect(result[0]).toBe(expectedMetarDataFirstEntry);
-      // @ts-ignore
       expect(result[1]).toBe(expectedMetarDataLastEntry);
+      // @ts-ignore
+      expect(result[0]).toBe(expectedMetarDataFirstEntry);
 
       done();
     })
