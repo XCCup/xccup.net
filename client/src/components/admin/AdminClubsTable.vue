612677e6833f0f66c650e9502b4a041b9156e8d2
KaiWissel <kai.wpkt@gmail.com>
2024-03-18 22:15:14 +0100

    Show map pos in admin panel

--- client/src/components/admin/AdminClubsTable.vue
+++ client/src/components/admin/AdminClubsTable.vue
@@ -5,6 +5,7 @@
       <th>Website</th>
       <th>Kontakt</th>
       <th>Club in</th>
+      <th>Karte</th>
       <th>Letzte Änderung</th>
       <th></th>
     </thead>
@@ -24,6 +25,7 @@
           </p>
         </td>
         <td>{{ beautifySeasonsForTable(club.participantInSeasons) }}</td>
+        <td>{{ club.mapPosition?.lat + ", " + club.mapPosition?.lon }}</td>
         <td>
           <BaseDate :timestamp="club.updatedAt" />
         </td>
