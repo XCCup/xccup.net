73211af8039aa09cded519772447cb5851253ad1
Stephan Schöpe <socialcrap+github@posteo.de>
2024-09-28 08:50:59 +0200

    Reapply "html semantics"
    This reverts commit c36eaa21538bdb760213a901a5bf30a85aca6bbd.

--- client/src/components/ResultsTableOverall.vue
+++ client/src/components/ResultsTableOverall.vue
@@ -4,33 +4,35 @@
     <div v-if="flights?.length > 0" v-memo="[flights]" class="table-responsive">
       <table class="table table-striped table-hover text-sm">
         <thead>
-          <TableSortHead
-            content="Datum"
-            column-object-key="takeoffTime"
-            :current-sort-column-key="currentSortColumnKey"
-            @head-sort-changed="handleSortChange"
-          />
-          <th class="hide-on-xs"></th>
-          <th>Name</th>
-          <th scope="col" class="hide-on-md">Verein</th>
-          <th scope="col" class="hide-on-md">Team</th>
-          <th class="hide-on-sm">Startplatz</th>
-          <th scope="col" class="hide-on-sm">Gerät</th>
-          <TableSortHead
-            content="Strecke"
-            column-object-key="flightDistance"
-            :current-sort-column-key="currentSortColumnKey"
-            @head-sort-changed="handleSortChange"
-          />
-          <TableSortHead
-            content="Punkte"
-            column-object-key="flightPoints"
-            :current-sort-column-key="currentSortColumnKey"
-            class="hide-on-sm"
-            @head-sort-changed="handleSortChange"
-          />
-          <th class="hide-on-xs"></th>
-          <th class="hide-on-md"></th>
+          <tr>
+            <TableSortHead
+              content="Datum"
+              column-object-key="takeoffTime"
+              :current-sort-column-key="currentSortColumnKey"
+              @head-sort-changed="handleSortChange"
+            />
+            <th class="hide-on-xs"></th>
+            <th>Name</th>
+            <th scope="col" class="hide-on-md">Verein</th>
+            <th scope="col" class="hide-on-md">Team</th>
+            <th class="hide-on-sm">Startplatz</th>
+            <th scope="col" class="hide-on-sm">Gerät</th>
+            <TableSortHead
+              content="Strecke"
+              column-object-key="flightDistance"
+              :current-sort-column-key="currentSortColumnKey"
+              @head-sort-changed="handleSortChange"
+            />
+            <TableSortHead
+              content="Punkte"
+              column-object-key="flightPoints"
+              :current-sort-column-key="currentSortColumnKey"
+              class="hide-on-sm"
+              @head-sort-changed="handleSortChange"
+            />
+            <th class="hide-on-xs"></th>
+            <th class="hide-on-md"></th>
+          </tr>
         </thead>
         <tbody>
           <tr
@@ -42,19 +44,21 @@
           >
             <td>
               <table>
-                <tr>
-                  <td>
-                    <BaseDate
-                      :timestamp="flight.takeoffTime"
-                      :date-format="$route.params.year ? 'dd.MM' : 'dd.MM.yy'"
-                    />
-                  </td>
-                </tr>
-                <tr class="d-md-none">
-                  <td class="fw-lighter max-width-11ch">
-                    {{ flight.takeoff?.name }}
-                  </td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <td>
+                      <BaseDate
+                        :timestamp="flight.takeoffTime"
+                        :date-format="$route.params.year ? 'dd.MM' : 'dd.MM.yy'"
+                      />
+                    </td>
+                  </tr>
+                  <tr class="d-md-none">
+                    <td class="fw-lighter max-width-11ch">
+                      {{ flight.takeoff?.name }}
+                    </td>
+                  </tr>
+                </tbody>
               </table>
             </td>
             <td class="hide-on-xs px-0 mx-0">
@@ -63,23 +67,27 @@
             </td>
             <td>
               <table>
-                <tr>
-                  <td>
-                    <PersonalBestIcon v-if="flight.isNewPersonalBest" />
+                <tbody>
+                  <tr>
+                    <td>
+                      <PersonalBestIcon v-if="flight.isNewPersonalBest" />
 
-                    <strong>
-                      {{
-                        flight.user?.firstName + " " + flight.user?.lastName
-                      }}</strong
-                    >
-                  </td>
-                </tr>
-                <tr class="d-md-none">
-                  <td scope="col">
-                    <RankingClass :ranking-class="flight.glider?.gliderClass" />
-                    {{ flight.glider?.model }}
-                  </td>
-                </tr>
+                      <strong>
+                        {{
+                          flight.user?.firstName + " " + flight.user?.lastName
+                        }}</strong
+                      >
+                    </td>
+                  </tr>
+                  <tr class="d-md-none">
+                    <td scope="col">
+                      <RankingClass
+                        :ranking-class="flight.glider?.gliderClass"
+                      />
+                      {{ flight.glider?.model }}
+                    </td>
+                  </tr>
+                </tbody>
               </table>
             </td>
             <td scope="col" class="hide-on-md">
@@ -103,17 +111,19 @@
             </td>
             <td>
               <table>
-                <tr>
-                  <td class="no-line-break">
-                    {{ Math.floor(flight.flightDistance) }} km
-                    <FlightTypeIcon :flight-type="flight.flightType" />
-                  </td>
-                </tr>
-                <tr class="d-md-none">
-                  <td class="fw-lighter no-line-break">
-                    {{ flight.flightPoints }} P
-                  </td>
-                </tr>
+                <tbody>
+                  <tr>
+                    <td class="no-line-break">
+                      {{ Math.floor(flight.flightDistance) }} km
+                      <FlightTypeIcon :flight-type="flight.flightType" />
+                    </td>
+                  </tr>
+                  <tr class="d-md-none">
+                    <td class="fw-lighter no-line-break">
+                      {{ flight.flightPoints }} P
+                    </td>
+                  </tr>
+                </tbody>
               </table>
             </td>
 
