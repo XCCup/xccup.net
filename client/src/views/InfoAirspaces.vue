b91c7ffa264c9927699382b63a6cc2391e9a2413
KaiWissel <kai.wpkt@gmail.com>
2025-01-04 22:56:04 +0100

    reset client eslint to v8

--- client/src/views/InfoAirspaces.vue
+++ client/src/views/InfoAirspaces.vue
@@ -1,14 +1,10 @@
 <template>
   <!-- eslint-disable-next-line vue/no-v-html -->
-  <div
-    class="container py-2"
-    style="max-width: 800px"
-    v-html="marked(markdown)"
-  ></div>
+  <div class="container py-2" style="max-width: 800px" v-html="marked(markdown)"></div>
 </template>
 <script setup lang="ts">
-import { marked } from "marked";
 import markdown from "@/blog/airspace-info.md?raw";
+import { marked } from "marked";
 import { setWindowName } from "../helper/utils";
 
 setWindowName("Von Lufträumen, Höhenmessungen und Standardatmospähren");
